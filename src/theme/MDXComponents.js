a6fd6436a50a8db54f456d636e7e65c84f0d22d9
Alexey Magdich <56426143+alexeymagdich-tigera@users.noreply.github.com>
2022-08-08 23:45:32 +0200

    [DOCS-593] Add `Variables` component (#2)

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MDXComponents from '@theme-original/MDXComponents';
+
+import Variables from '@site/src/components/Variables';
+
+export default {
+  ...MDXComponents,
+
+  variables: Variables,
+};
