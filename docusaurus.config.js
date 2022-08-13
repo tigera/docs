a43d5d2c986735ae05653f2a30e1203480b86b9b
Doug Collier <doug@tigera.io>
2022-08-13 08:19:17 -0500

    remove unnecessary comments

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,11 +16,6 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.png',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  // organizationName: 'tigera', // Usually your GitHub org/username.
-  // projectName: 'docs', // Usually your repo name.
-
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
@@ -36,22 +31,12 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/tigera/docs/',
+          editUrl: 'https://github.com/tigera/docs/',
           beforeDefaultRemarkPlugins: [
             variablesPlugin,
           ],
         },
         blog: false,
-        // {
-        //   showReadingTime: true,
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        // },
         theme: {
           customCss: [
             require.resolve('./src/css/custom.css'),
