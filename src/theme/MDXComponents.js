4749b33c9c0cb34f258674cc17b2475dc761e47a
Alexey Magdich <itechart.aliaksei.mahdzich@external.tigera.io>
2022-08-29 16:10:26 +0200

    DOCS-626: Add `HostEndpointsUpgrade` partial component

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -2,12 +2,14 @@ import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
 import ReqsSys from '@site/src/components/partials/reqs-sys';
 import ReqsKernel from '@site/src/components/partials/reqs-kernel';
+import HostEndpointsUpgrade from '@site/src/components/partials/hostendpoints-upgrade';
 
 export default {
     // Re-use the default mapping
     ...MDXComponents,
 
     // all of our partials
-    ReqsSys: ReqsSys,
-    ReqsKernel: ReqsKernel,
+    ReqsSys,
+    ReqsKernel,
+    HostEndpointsUpgrade,
 };
