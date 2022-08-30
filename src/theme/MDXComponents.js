1ff152bfd75f2eecc65b5ae401494da0488d6aee
Denis Al-Khelali <denis.al-khelali@itechart-group.com>
2022-08-31 02:34:40 +0300

    [DOCS-641] Partials - install-openshift-before-you-begin.md (#13)
    Co-authored-by: Doug Collier <doug@tigera.io>

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -4,6 +4,7 @@ import ReqsSys from '@site/src/components/partials/reqs-sys';
 import ReqsKernel from '@site/src/components/partials/reqs-kernel';
 import AutoHostendpointsMigrate from '@site/src/components/partials/auto-hostendpoints-migrate';
 import HostEndpointsUpgrade from '@site/src/components/partials/hostendpoints-upgrade';
+import InstallOpenshiftBeforeYouBegin from '@site/src/components/partials/install-openshift-before-you-begin';
 import CalicoWindowsInstall from '@site/src/components/partials/calico-windows-install';
 import PodCidrSed from '@site/src/components/partials/pod-cidr-sed';
 import EnvironmentFile from '@site/src/components/partials/environment-file';
@@ -16,6 +17,7 @@ export default {
     ReqsSys,
     ReqsKernel,
     HostEndpointsUpgrade,
+    InstallOpenshiftBeforeYouBegin,
     CalicoWindowsInstall,
     PodCidrSed,
     EnvironmentFile,
