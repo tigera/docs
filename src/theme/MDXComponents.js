469293ad21ab8b3e010a3c19188f563f505c7f46
Denis Al-Khelali <denis.al-khelali@itechart-group.com>
2022-08-31 02:22:26 +0300

    [DOCS-623] Partials - auto-hostendpoints-migrate (#8)
    * [DOCS-623] Partials - auto-hostendpoints-migrate
    
    * [DOCS-623] Use <CodeBlock>

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
 import ReqsSys from '@site/src/components/partials/reqs-sys';
 import ReqsKernel from '@site/src/components/partials/reqs-kernel';
+import AutoHostendpointsMigrate from '@site/src/components/partials/auto-hostendpoints-migrate';
 import HostEndpointsUpgrade from '@site/src/components/partials/hostendpoints-upgrade';
 
 export default {
@@ -12,4 +13,5 @@ export default {
     ReqsSys,
     ReqsKernel,
     HostEndpointsUpgrade,
+    AutoHostendpointsMigrate,
 };
