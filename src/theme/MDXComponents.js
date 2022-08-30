d8f1bfd4bfcf27d0f8c0c6b2e9af5787cdf3e978
Alexey Magdich <56426143+alexeymagdich-tigera@users.noreply.github.com>
2022-08-31 01:29:33 +0200

    DOCS-627: Add `PodCidrSed` component (#11)
    * DOCS-627: Add `PodCidrSed` component
    
    * DOCS-626: Add comment about indentation
    
    * DOCS-627: Format file
    
    Co-authored-by: Doug Collier <doug@tigera.io>

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -4,6 +4,7 @@ import ReqsSys from '@site/src/components/partials/reqs-sys';
 import ReqsKernel from '@site/src/components/partials/reqs-kernel';
 import AutoHostendpointsMigrate from '@site/src/components/partials/auto-hostendpoints-migrate';
 import HostEndpointsUpgrade from '@site/src/components/partials/hostendpoints-upgrade';
+import PodCidrSed from '@site/src/components/partials/pod-cidr-sed';
 import EnvironmentFile from '@site/src/components/partials/environment-file';
 
 export default {
@@ -14,6 +15,7 @@ export default {
     ReqsSys,
     ReqsKernel,
     HostEndpointsUpgrade,
+    PodCidrSed,
     EnvironmentFile,
     AutoHostendpointsMigrate,
 };
