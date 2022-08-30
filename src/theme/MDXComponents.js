8deef69d44a98a4921f0b13a79a3f3395fdd2dac
Denis Al-Khelali <denis.al-khelali@itechart-group.com>
2022-08-31 02:36:33 +0300

    [DOCS-630] Partials - enterprise-icon (#14)
    Co-authored-by: Doug Collier <doug@tigera.io>

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,9 +1,11 @@
 import React from 'react';
+
 import MDXComponents from '@theme-original/MDXComponents';
 import ReqsSys from '@site/src/components/partials/reqs-sys';
 import ReqsKernel from '@site/src/components/partials/reqs-kernel';
 import AutoHostendpointsMigrate from '@site/src/components/partials/auto-hostendpoints-migrate';
 import HostEndpointsUpgrade from '@site/src/components/partials/hostendpoints-upgrade';
+import EnterpriseLink from '@site/src/components/partials/enterprise-link';
 import InstallOpenshiftBeforeYouBegin from '@site/src/components/partials/install-openshift-before-you-begin';
 import CalicoWindowsInstall from '@site/src/components/partials/calico-windows-install';
 import PodCidrSed from '@site/src/components/partials/pod-cidr-sed';
@@ -17,6 +19,7 @@ export default {
     ReqsSys,
     ReqsKernel,
     HostEndpointsUpgrade,
+    EnterpriseLink,
     InstallOpenshiftBeforeYouBegin,
     CalicoWindowsInstall,
     PodCidrSed,
