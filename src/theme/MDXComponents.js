d7f89baa9b15796283a643ce01efc48b715344cd
Alexey Magdich <56426143+alexeymagdich-tigera@users.noreply.github.com>
2022-08-31 01:32:28 +0200

    DOCS-624: Add `CalicoWindowsInstall` partial component (#12)
    Co-authored-by: Doug Collier <doug@tigera.io>

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -4,6 +4,7 @@ import ReqsSys from '@site/src/components/partials/reqs-sys';
 import ReqsKernel from '@site/src/components/partials/reqs-kernel';
 import AutoHostendpointsMigrate from '@site/src/components/partials/auto-hostendpoints-migrate';
 import HostEndpointsUpgrade from '@site/src/components/partials/hostendpoints-upgrade';
+import CalicoWindowsInstall from '@site/src/components/partials/calico-windows-install';
 import PodCidrSed from '@site/src/components/partials/pod-cidr-sed';
 import EnvironmentFile from '@site/src/components/partials/environment-file';
 
@@ -15,6 +16,7 @@ export default {
     ReqsSys,
     ReqsKernel,
     HostEndpointsUpgrade,
+    CalicoWindowsInstall,
     PodCidrSed,
     EnvironmentFile,
     AutoHostendpointsMigrate,
