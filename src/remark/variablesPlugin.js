6019ce8442d97894b70f768ed07b38388e84b720
Doug Collier <doug@tigera.io>
2022-09-01 08:58:13 -0500

    add support for context vars, can override including multi-path support which picks up partials

--- src/remark/variablesPlugin.js
+++ src/remark/variablesPlugin.js
@@ -1,51 +1,64 @@
-const path = require("path");
-const visit = require("unist-util-visit");
+const path = require("path")
+const visit = require("unist-util-visit")
 
-const variables = require(path.resolve("variables"));
-const objProp = require(path.resolve("src/utils/objProp"));
-
-const pathPrefixes = {
-	cloud: "/docs/calico-cloud/",
-	enterprise: "/docs/calico-enterprise/",
-	openSource: "/docs/calico/",
-};
+const variables = require(path.resolve("variables"))
+const objProp = require(path.resolve("src/utils/objProp"))
+const varRegex = RegExp(/\{\{[ \t]*([\w.]+)[ \t]*}}/, 'g')
 
 function variablesPlugin(_options) {
 	async function transformer(tree, file) {
-		const posixFriendlyPath = convertToPosixFriendlyPath(file.path);
-		const productVariables = posixFriendlyPath.includes(pathPrefixes.cloud)
-			? variables.cloud
-			: posixFriendlyPath.includes(pathPrefixes.enterprise)
-				? variables.enterprise
-				: posixFriendlyPath.includes(pathPrefixes.openSource)
-					? variables.openSource
-					: null;
+		const contextVariables = getContextVariables(file, variables)
+		visit(tree, () => true, (node) => {
+			for (let prop in node) {
+				if (!Object.prototype.hasOwnProperty.call(node, prop)) continue
+				if (prop === 'type' || typeof node[prop] !== 'string') continue
 
-		if (!productVariables) {
-			return;
-		}
-
-		visit(tree, ["text", "jsx", "code", "link", "inlineCode"], (node) => {
-			const valueKey = node.type === "link" ? "url" : "value";
-
-			node[valueKey] = node[valueKey].replaceAll(
-				/{{([\w.]+)}}/g,
-				(match, varName) => {
-					const varValue = objProp(productVariables, varName);
-					if (!varValue) {
-						return match;
-					}
-					return varValue;
-				}
-			);
-		});
+				node[prop] = node[prop].replaceAll(varRegex, (match, varName) => {
+						let varValue
+						for (let cv of contextVariables) {
+							varValue = objProp(cv, varName)
+							if (varValue) return String(varValue)
+						}
+						varValue = objProp(variables.global, varName)
+						if (varValue) return String(varValue)
+						return match
+					},
+				)
+			}
+		})
 	}
 
-	return transformer;
+	return transformer
+}
+
+function getContextVariables(file, variables) {
+	let cvars = []
+	const dpName = 'docsPathPrefix'
+	const posixFriendlyPath = convertToPosixFriendlyPath(file.path)
+
+	for (let p in variables) {
+		if (!Object.prototype.hasOwnProperty.call(variables, p)) continue
+		if (typeof variables[p] === 'object') {
+			if (!Object.prototype.hasOwnProperty.call(variables[p], dpName)) continue
+			const docsPathPrefix = variables[p][dpName]
+			if (Array.isArray(docsPathPrefix)) {
+				for (let dpp of docsPathPrefix) {
+					if (posixFriendlyPath.includes(dpp)) {
+						cvars[cvars.length] = variables[p]
+					}
+				}
+			} else if (typeof docsPathPrefix === 'string') {
+				if (posixFriendlyPath.includes(docsPathPrefix)) {
+					cvars[cvars.length] = variables[p]
+				}
+			}
+		}
+	}
+	return cvars
 }
 
 function convertToPosixFriendlyPath(maybeWindowsPath) {
-	return maybeWindowsPath.split(path.sep).join(path.posix.sep);
+	return maybeWindowsPath.split(path.sep).join(path.posix.sep)
 }
 
-module.exports = variablesPlugin;
+module.exports = variablesPlugin
--- variables.js
+++ variables.js
@@ -2,8 +2,12 @@ const variables = {
 	cloud: {
 		prodname: "Calico Cloud",
 		prodnamedash: "calico-cloud",
+		docsPathPrefix: [
+			'/docs/calico-cloud/',
+			'/docs/_includes/calico-cloud/',
+		],
 		prodnameWindows: "Calico Enterprise for Windows",
-		rootDirWindows: "C:TigeraCalico",
+		rootDirWindows: "C:\\TigeraCalico",
 		nodecontainer: "cnx-node",
 		noderunning: "calico-node",
 		clouddownloadurl: "https://installer.calicocloud.io/manifests/v3.14.1-1",
@@ -11,27 +15,37 @@ const variables = {
 	enterprise: {
 		prodname: "Calico Enterprise",
 		prodnamedash: "calico-enterprise",
+		docsPathPrefix: [
+			'/docs/calico-enterprise/',
+			'/docs/_includes/calico-enterprise/',
+		],
 		prodnameWindows: "Calico Enterprise for Windows",
 		downloadsurl: "https://downloads.tigera.io",
 		nodecontainer: "cnx-node",
 		noderunning: "calico-node",
-		rootDirWindows: "C:\TigeraCalico",
+		rootDirWindows: "C:\\TigeraCalico",
 	},
 	openSource: {
 		prodname: "Calico",
 		prodnamedash: "calico",
+		docsPathPrefix: [
+			'/docs/calico/',
+			'/docs/_includes/calico/',
+		],
 		prodnameWindows: "Calico for Windows",
 		nodecontainer: "calico/node",
 		noderunning: "calico-node",
-		rootDirWindows: "C:\CalicoWindows",
+		rootDirWindows: "C:\\CalicoWindows",
 	},
-	orchestrators: {
-		All: 'All',
-		Kubernetes: 'Kubernetes',
-		OpenShift: 'OpenShift',
-		OpenStack: 'OpenStack',
-		HostProtection: 'host protection',
-		'host protection': 'host protection',
+	global: {
+		orchestrators: {
+			All: 'All',
+			Kubernetes: 'Kubernetes',
+			OpenShift: 'OpenShift',
+			OpenStack: 'OpenStack',
+			HostProtection: 'host protection',
+			'host protection': 'host protection',
+		},
 	},
 };
 
