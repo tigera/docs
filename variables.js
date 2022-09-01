657337c98d88ba873e3def5ea8422815c0221010
Doug Collier <doug@tigera.io>
2022-09-01 15:21:40 -0500

    add support for context vars, can override including multi-path suppo… (#16)
    * add support for context vars, can override including multi-path support which picks up partials
    
    * format with prettier
    
    * add some comments
    
    * minor updates

--- variables.js
+++ variables.js
@@ -1,38 +1,55 @@
 const variables = {
-	cloud: {
-		prodname: "Calico Cloud",
-		prodnamedash: "calico-cloud",
-		prodnameWindows: "Calico Enterprise for Windows",
-		rootDirWindows: "C:TigeraCalico",
-		nodecontainer: "cnx-node",
-		noderunning: "calico-node",
-		clouddownloadurl: "https://installer.calicocloud.io/manifests/v3.14.1-1",
-	},
-	enterprise: {
-		prodname: "Calico Enterprise",
-		prodnamedash: "calico-enterprise",
-		prodnameWindows: "Calico Enterprise for Windows",
-		downloadsurl: "https://downloads.tigera.io",
-		nodecontainer: "cnx-node",
-		noderunning: "calico-node",
-		rootDirWindows: "C:\TigeraCalico",
-	},
-	openSource: {
-		prodname: "Calico",
-		prodnamedash: "calico",
-		prodnameWindows: "Calico for Windows",
-		nodecontainer: "calico/node",
-		noderunning: "calico-node",
-		rootDirWindows: "C:\CalicoWindows",
-	},
-	orchestrators: {
-		All: 'All',
-		Kubernetes: 'Kubernetes',
-		OpenShift: 'OpenShift',
-		OpenStack: 'OpenStack',
-		HostProtection: 'host protection',
-		'host protection': 'host protection',
-	},
+  // This is an example of how to override variables. Bear in mind that 1) the
+  // 'docsPathPrefix' is necessary, except for 'global' object. And, 2) the
+  // order is important as the first objects listed here will take precedence
+  // over the objects listed further down, as long as the paths match.
+  //
+  // override: {
+  // 	docsPathPrefix: ["/docs/calico-cloud/new-folder-example/"],
+  // 	nodecontainer: 'cnx-node-override'
+  // },
+  cloud: {
+    prodname: "Calico Cloud",
+    prodnamedash: "calico-cloud",
+    docsPathPrefix: ["/docs/calico-cloud/", "/docs/_includes/calico-cloud/"],
+    prodnameWindows: "Calico Enterprise for Windows",
+    rootDirWindows: "C:\\TigeraCalico",
+    nodecontainer: "cnx-node",
+    noderunning: "calico-node",
+    clouddownloadurl: "https://installer.calicocloud.io/manifests/v3.14.1-1",
+  },
+  enterprise: {
+    prodname: "Calico Enterprise",
+    prodnamedash: "calico-enterprise",
+    docsPathPrefix: [
+      "/docs/calico-enterprise/",
+      "/docs/_includes/calico-enterprise/",
+    ],
+    prodnameWindows: "Calico Enterprise for Windows",
+    downloadsurl: "https://downloads.tigera.io",
+    nodecontainer: "cnx-node",
+    noderunning: "calico-node",
+    rootDirWindows: "C:\\TigeraCalico",
+  },
+  openSource: {
+    prodname: "Calico",
+    prodnamedash: "calico",
+    docsPathPrefix: ["/docs/calico/", "/docs/_includes/calico/"],
+    prodnameWindows: "Calico for Windows",
+    nodecontainer: "calico/node",
+    noderunning: "calico-node",
+    rootDirWindows: "C:\\CalicoWindows",
+  },
+  global: {
+    orchestrators: {
+      All: "All",
+      Kubernetes: "Kubernetes",
+      OpenShift: "OpenShift",
+      OpenStack: "OpenStack",
+      HostProtection: "host protection",
+      "host protection": "host protection",
+    },
+  },
 };
 
 module.exports = variables;
