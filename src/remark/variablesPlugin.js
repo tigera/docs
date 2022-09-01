a725d4e6507f7658ff7a4125fed14eeadd94d348
Doug Collier <doug@tigera.io>
2022-09-01 14:00:19 -0500

    format with prettier

--- src/remark/variablesPlugin.js
+++ src/remark/variablesPlugin.js
@@ -1,64 +1,66 @@
-const path = require("path")
-const visit = require("unist-util-visit")
+const path = require("path");
+const visit = require("unist-util-visit");
 
-const variables = require(path.resolve("variables"))
-const objProp = require(path.resolve("src/utils/objProp"))
-const varRegex = RegExp(/\{\{[ \t]*([\w.]+)[ \t]*}}/, 'g')
+const variables = require(path.resolve("variables"));
+const objProp = require(path.resolve("src/utils/objProp"));
+const varRegex = RegExp(/\{\{[ \t]*([\w.]+)[ \t]*}}/, "g");
 
 function variablesPlugin(_options) {
-	async function transformer(tree, file) {
-		const contextVariables = getContextVariables(file, variables)
-		visit(tree, () => true, (node) => {
-			for (let prop in node) {
-				if (!Object.prototype.hasOwnProperty.call(node, prop)) continue
-				if (prop === 'type' || typeof node[prop] !== 'string') continue
+  async function transformer(tree, file) {
+    const contextVariables = getContextVariables(file, variables);
+    visit(
+      tree,
+      () => true,
+      (node) => {
+        for (let prop in node) {
+          if (!Object.prototype.hasOwnProperty.call(node, prop)) continue;
+          if (prop === "type" || typeof node[prop] !== "string") continue;
 
-				node[prop] = node[prop].replaceAll(varRegex, (match, varName) => {
-						let varValue
-						for (let cv of contextVariables) {
-							varValue = objProp(cv, varName)
-							if (varValue) return String(varValue)
-						}
-						varValue = objProp(variables.global, varName)
-						if (varValue) return String(varValue)
-						return match
-					},
-				)
-			}
-		})
-	}
+          node[prop] = node[prop].replaceAll(varRegex, (match, varName) => {
+            let varValue;
+            for (let cv of contextVariables) {
+              varValue = objProp(cv, varName);
+              if (varValue) return String(varValue);
+            }
+            varValue = objProp(variables.global, varName);
+            if (varValue) return String(varValue);
+            return match;
+          });
+        }
+      }
+    );
+  }
 
-	return transformer
+  return transformer;
 }
 
 function getContextVariables(file, variables) {
-	let cvars = []
-	const dpName = 'docsPathPrefix'
-	const posixFriendlyPath = convertToPosixFriendlyPath(file.path)
+  let cvars = [];
+  const dpName = "docsPathPrefix";
+  const posixFriendlyPath = convertToPosixFriendlyPath(file.path);
 
-	for (let p in variables) {
-		if (!Object.prototype.hasOwnProperty.call(variables, p)) continue
-		if (typeof variables[p] === 'object') {
-			if (!Object.prototype.hasOwnProperty.call(variables[p], dpName)) continue
-			const docsPathPrefix = variables[p][dpName]
-			if (Array.isArray(docsPathPrefix)) {
-				for (let dpp of docsPathPrefix) {
-					if (posixFriendlyPath.includes(dpp)) {
-						cvars[cvars.length] = variables[p]
-					}
-				}
-			} else if (typeof docsPathPrefix === 'string') {
-				if (posixFriendlyPath.includes(docsPathPrefix)) {
-					cvars[cvars.length] = variables[p]
-				}
-			}
-		}
-	}
-	return cvars
+  for (let p in variables) {
+    if (!Object.prototype.hasOwnProperty.call(variables, p)) continue;
+    if (typeof variables[p] !== "object") continue;
+    if (!Object.prototype.hasOwnProperty.call(variables[p], dpName)) continue;
+    const docsPathPrefix = variables[p][dpName];
+    if (Array.isArray(docsPathPrefix)) {
+      for (let dpp of docsPathPrefix) {
+        if (posixFriendlyPath.includes(dpp)) {
+          cvars[cvars.length] = variables[p];
+        }
+      }
+    } else if (typeof docsPathPrefix === "string") {
+      if (posixFriendlyPath.includes(docsPathPrefix)) {
+        cvars[cvars.length] = variables[p];
+      }
+    }
+  }
+  return cvars;
 }
 
 function convertToPosixFriendlyPath(maybeWindowsPath) {
-	return maybeWindowsPath.split(path.sep).join(path.posix.sep)
+  return maybeWindowsPath.split(path.sep).join(path.posix.sep);
 }
 
-module.exports = variablesPlugin
+module.exports = variablesPlugin;
--- variables.js
+++ variables.js
@@ -1,52 +1,46 @@
 const variables = {
-	cloud: {
-		prodname: "Calico Cloud",
-		prodnamedash: "calico-cloud",
-		docsPathPrefix: [
-			'/docs/calico-cloud/',
-			'/docs/_includes/calico-cloud/',
-		],
-		prodnameWindows: "Calico Enterprise for Windows",
-		rootDirWindows: "C:\\TigeraCalico",
-		nodecontainer: "cnx-node",
-		noderunning: "calico-node",
-		clouddownloadurl: "https://installer.calicocloud.io/manifests/v3.14.1-1",
-	},
-	enterprise: {
-		prodname: "Calico Enterprise",
-		prodnamedash: "calico-enterprise",
-		docsPathPrefix: [
-			'/docs/calico-enterprise/',
-			'/docs/_includes/calico-enterprise/',
-		],
-		prodnameWindows: "Calico Enterprise for Windows",
-		downloadsurl: "https://downloads.tigera.io",
-		nodecontainer: "cnx-node",
-		noderunning: "calico-node",
-		rootDirWindows: "C:\\TigeraCalico",
-	},
-	openSource: {
-		prodname: "Calico",
-		prodnamedash: "calico",
-		docsPathPrefix: [
-			'/docs/calico/',
-			'/docs/_includes/calico/',
-		],
-		prodnameWindows: "Calico for Windows",
-		nodecontainer: "calico/node",
-		noderunning: "calico-node",
-		rootDirWindows: "C:\\CalicoWindows",
-	},
-	global: {
-		orchestrators: {
-			All: 'All',
-			Kubernetes: 'Kubernetes',
-			OpenShift: 'OpenShift',
-			OpenStack: 'OpenStack',
-			HostProtection: 'host protection',
-			'host protection': 'host protection',
-		},
-	},
+  cloud: {
+    prodname: "Calico Cloud",
+    prodnamedash: "calico-cloud",
+    docsPathPrefix: ["/docs/calico-cloud/", "/docs/_includes/calico-cloud/"],
+    prodnameWindows: "Calico Enterprise for Windows",
+    rootDirWindows: "C:\\TigeraCalico",
+    nodecontainer: "cnx-node",
+    noderunning: "calico-node",
+    clouddownloadurl: "https://installer.calicocloud.io/manifests/v3.14.1-1",
+  },
+  enterprise: {
+    prodname: "Calico Enterprise",
+    prodnamedash: "calico-enterprise",
+    docsPathPrefix: [
+      "/docs/calico-enterprise/",
+      "/docs/_includes/calico-enterprise/",
+    ],
+    prodnameWindows: "Calico Enterprise for Windows",
+    downloadsurl: "https://downloads.tigera.io",
+    nodecontainer: "cnx-node",
+    noderunning: "calico-node",
+    rootDirWindows: "C:\\TigeraCalico",
+  },
+  openSource: {
+    prodname: "Calico",
+    prodnamedash: "calico",
+    docsPathPrefix: ["/docs/calico/", "/docs/_includes/calico/"],
+    prodnameWindows: "Calico for Windows",
+    nodecontainer: "calico/node",
+    noderunning: "calico-node",
+    rootDirWindows: "C:\\CalicoWindows",
+  },
+  global: {
+    orchestrators: {
+      All: "All",
+      Kubernetes: "Kubernetes",
+      OpenShift: "OpenShift",
+      OpenStack: "OpenStack",
+      HostProtection: "host protection",
+      "host protection": "host protection",
+    },
+  },
 };
 
 module.exports = variables;
