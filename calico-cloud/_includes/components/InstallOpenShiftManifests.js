9ac8860a58cd23c8e2218a3a4ac199fa26434042
Christopher Tauchen <chris.tauchen@tigera.io>
2022-12-03 00:43:30 +0000

    content updates

---
+++ calico-cloud/_includes/components/InstallOpenShiftManifests.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import CodeBlock from '@theme/CodeBlock';
+
+import { prodname, filesUrl } from '../../variables';
+
+export default function InstallOpenShiftManifests() {
+  return (
+    <>
+      <p>Download the {prodname} manifests for OpenShift and add them to the generated manifests directory:</p>
+      <CodeBlock language='bash-plain-text'>
+        {`mkdir calico
+wget -qO- ${filesUrl}/manifests/ocp.tgz | tar xvz --strip-components=1 -C calico
+cp calico/* manifests/`}
+      </CodeBlock>
+    </>
+  );
+}
