dd127583a170a2236b6ee9f2df86dc80616b17b4
Christopher Tauchen <chris.tauchen@tigera.io>
2022-12-16 15:59:53 +0000

    Adds links to Tigera menu and other bits and pieces

--- src/theme/Navbar/Logo/index.js
+++ src/theme/Navbar/Logo/index.js
@@ -15,6 +15,22 @@ export default function LogoWrapper(props) {
           label: 'Pricing',
           to: 'https://www.tigera.io/tigera-products/calico-cloud-pricing/',
         },
+        {
+          label: 'Why Calico?',
+          to: 'https://www.tigera.io/tigera-products/why-calico/',
+        },
+        {
+          label: 'Blog',
+          to: 'https://www.tigera.io/blog/',
+        },
+        {
+          label: 'Project Calico',
+          to: 'https://www.tigera.io/project-calico/',
+        },
+        {
+          label: 'Resource center',
+          to: 'https://www.tigera.io/resources/',
+        },
       ]}
       label={<Logo {...props} />}
       className='logo-wrapper'
