147e92f85acfb02ce6f4d177d2ddad13f7eed0be
Doug Collier <doug@tigera.io>
2022-12-21 17:36:51 -0600

    fix breadcrumb for calico cloud when you click on center home page link

--- src/utils/useProductId.js
+++ src/utils/useProductId.js
@@ -3,11 +3,13 @@ const { useLocation } = require('@docusaurus/router');
 export function useProductId() {
   const { pathname } = useLocation();
 
-  if (pathname.startsWith('/calico/')) {
+  if (pathname.startsWith('/calico/') || pathname === '/calico') {
     return 'calico';
-  } else if (pathname.startsWith('/calico-cloud/')) {
+  } else if (pathname.startsWith('/calico-cloud/') || pathname ===
+    '/calico-cloud') {
     return 'calico-cloud';
-  } else if (pathname.startsWith('/calico-enterprise/')) {
+  } else if (pathname.startsWith('/calico-enterprise/') || pathname ===
+    '/calico-enterprise') {
     return 'calico-enterprise';
   }
 }
