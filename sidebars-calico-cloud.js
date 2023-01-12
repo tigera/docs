d7fb62f7bf558c38dd888f339942b58d20b9a0f8
Christopher Tauchen <chris.tauchen@tigera.io>
2023-01-12 16:34:24 +0000

    DOCS-1172: Fixing changed file name in sidebars

--- sidebars-calico-cloud.js
+++ sidebars-calico-cloud.js
@@ -108,7 +108,7 @@ module.exports = {
           label: 'Tiered network policy',
           link: {type: 'doc', id: 'network-policy/policy-tiers/index'},
           items: [
-            'network-policy/policy-tiers/policy-tiers',
+            'network-policy/policy-tiers/tiered-policy',
             //security/policy-tiers/policy-tutorial-ui'//
             'network-policy/policy-tiers/allow-tigera',
             'network-policy/policy-tiers/rbac-tiered-policies',
