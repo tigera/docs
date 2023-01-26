b7603ce19c49d7e64bb79fcdffc180b294c33989
Christopher Tauchen <chris.tauchen@tigera.io>
2023-01-26 17:27:44 +0000

    Cutting docs for CC based on CE 3.15

---
+++ calico-cloud_versioned_docs/version-3.15/variables.js
@@ -0,0 +1,26 @@
+const releases = require('./releases.json');
+
+const variables = {
+  releaseTitle: 'v3.14.1',
+  prodname: 'Calico Cloud',
+  prodnamedash: 'calico-cloud',
+  baseUrl: '/calico-cloud',
+  filesUrl: 'https://docs.calicocloud.io',
+  filesUrl_CE: 'https://downloads.tigera.io/ee/v3.15.0',
+  prodnameWindows: 'Calico Enterprise for Windows',
+  rootDirWindows: 'C:\\TigeraCalico',
+  nodecontainer: 'cnx-node',
+  noderunning: 'calico-node',
+  cloudversion: 'v3.14.1-16',
+  clouddownloadurl: 'https://installer.calicocloud.io/manifests/v3.14.1-16',
+  clouddownloadbase: 'https://installer.calicocloud.io',
+  tigeraOperator: releases[0]['tigera-operator'],
+  releases,
+  registry: 'quay.io/',
+  imageNames: {
+    node: 'tigera/cnx-node',
+    kubeControllers: 'tigera/kube-controllers',
+  },
+};
+
+module.exports = variables;
