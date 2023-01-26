3ca670839785e22d031d2d9dd19a45a065b07c8d
Doug Collier <doug@tigera.io>
2023-01-25 19:09:49 -0600

    cleanup link-checker

--- src/remark/linkCheckerPlugin.js
+++ src/remark/linkCheckerPlugin.js
@@ -1,9 +1,6 @@
 const visit = require('unist-util-visit');
 const linkCheck = require('link-check');
 const linkRegex = [
-  // new RegExp(
-  //   /\bhttps?:\/\/[\w.:/-]+?(\.ya?ml|\.ps1|\.tgz|\.sh|\.zip|\.css|\.js)\b/,
-  //   'g'),
   new RegExp(/https?:\/\/[-a-zA-Z0-9()@:%._+~#?&/=]+/, 'g'),
 ];
 const validURLRegex = new RegExp(
@@ -24,9 +21,15 @@ const skipRegex = [
   new RegExp(/\/manifests\/alp\/istio-inject-configmap-$/),
   new RegExp(/:\/\/transfer\.sh/),
   new RegExp(/:\/\/auth\.calicocloud\.io/),
+  new RegExp(/:\/\/example.com/),
+  new RegExp(/:\/\/www\.calicocloud\.io/),
 ];
-const LC = 'LINK-CHECK', ERR = `${LC} ERROR`;
-const DEAD = 'dead', SKIPPED = 'skipped',
+const skipLiteral = [
+  'https://web.archive.org/web/20150923231827/https://www.cisco.com/web/about/ac123/ac147/archived_issues/ipj_14-3/143_trill.html',
+  'https://web.archive.org/web/20210204031636/https://cumulusnetworks.com/blog/celebrating-ecmp-part-two/',
+  'https://www.fluentd.org/',
+];
+const LC = 'LINK-CHECK', DEAD = 'dead', SKIPPED = 'skipped',
   ALIVE = 'alive', ERROR = 'error', INVALID = 'invalid';
 const urlMap = new Map();
 const sys_errors = [];
@@ -34,7 +37,7 @@ const sys_errors = [];
 function linkCheckerPlugin(_options) {
   const linkCheckCallback = function(err, result) {
     if (err) {
-      sys_errors[sys_errors.length] = `${ERR}: ${err}`;
+      sys_errors[sys_errors.length] = `${LC} SYS-ERROR: ${err}`;
     } else if (result.err) {
       urlMap.set(result.link, { state: ERROR, msg: `${result.err}` });
     } else {
@@ -48,9 +51,10 @@ function linkCheckerPlugin(_options) {
       return true;
     }
     for (const sre of skipRegex) {
-      if (sre.test(url)) {
-        return true;
-      }
+      if (sre.test(url)) return true;
+    }
+    for (const l of skipLiteral) {
+      if (l === url) return true;
     }
     return false;
   }
@@ -63,13 +67,17 @@ function linkCheckerPlugin(_options) {
           for (const lre of linkRegex) {
             const matches = node[prop].matchAll(lre);
             for (const match of matches) {
-              const url = match[0].trim().replace(/\)$/, '');
+              const url = match[0].trim().replace(/(\)|\)\.)$/, '');
               if (urlMap.has(url)) continue;
               urlMap.set(url, SKIPPED); // init to skipped
               if (IsExcludedOrInvalid(url)) {
                 continue;
               }
-              const opts = { retryOn429: true, fallbackRetryDelay: '15s' };
+              const opts = {
+                retryOn429: true,
+                fallbackRetryDelay: '15s',
+                timeout: '30s',
+              };
               linkCheck(url, opts, linkCheckCallback);
             }
           }
@@ -82,6 +90,7 @@ function linkCheckerPlugin(_options) {
 }
 
 function postBuild() {
+  let exit = false;
   let skipped = 0, invalid = 0, alive = 0, dead = 0, error = 0;
   urlMap.forEach((v, k) => {
     if (typeof v === 'object' && v.state === ERROR) {
@@ -106,59 +115,53 @@ function postBuild() {
 
   if (invalid > 0) {
     console.info(
-      `\n\t[INFO] LINK-CHECK skipped ${invalid} invalid link(s). The list follows:`);
-    urlMap.forEach((v, k) => {
-      if (v === INVALID) {
-        console.warn(`\t${k} is ${v}`);
-      }
-    });
+      `\n\t[INFO] ${LC} skipped the following ${invalid} invalid link(s):`);
+    enumMap(v => v === INVALID, (v, k) => `\t${k} is ${v}`, 'info');
   }
 
   if (skipped > 0) {
     console.info(
-      `\n\t[INFO] LINK-CHECK skipped ${skipped} external link(s) due to built-in skip rules. The list follows:`);
-    urlMap.forEach((v, k) => {
-      if (v === SKIPPED) {
-        console.warn(`\t${k} was ${v}`);
-      }
-    });
+      `\n\t[INFO] ${LC} skipped the following ${skipped} external link(s) due to built-in skip rules:`);
+    enumMap(v => v === SKIPPED, (v, k) => `\t${k} was ${v}`, 'info');
   }
 
   if (dead > 0) {
     console.warn(
-      `\n\t[WARNING] LINK-CHECK found ${dead} dead external link(s). The list follows:`);
-    urlMap.forEach((v, k) => {
-      if (v === DEAD) {
-        console.warn(`\t${k} is ${v}`);
-      }
-    });
+      `\n\t[WARNING] ${LC} found the following ${dead} dead external link(s):`);
+    enumMap(v => v === DEAD, (v, k) => `\t${k} is ${v}`, 'warn');
   }
 
   if (error > 0) {
-    console.error(
-      `${LC} ERRORS: we experienced ${error} error(s). The list follows:`);
-    urlMap.forEach((v, k) => {
-      if (typeof v === 'object' && v.state === ERROR) {
-        console.warn(`\t${k} error: ${v.msg}`);
-      }
-    });
+    console.warn(`\n\t[ERROR] ${LC} hit the following ${error} error(s):`);
+    enumMap(v => typeof v === 'object' && v.state === ERROR,
+      (v, k) => `\t${k} error: ${v.msg}`, 'warn');
   }
-
-  let exit = false;
 
   if (sys_errors.length > 0) {
     exit = true;
     console.error(
-      `${LC} FATAL ERROR: we experienced ${sys_errors.length} system errors while link checking. The list follows:`);
-    for (const err of sys_errors) {
-      console.error(`\t${err}`);
-    }
+      `\n\t[FATAL] ${LC} FATAL ERROR(S): the following ${sys_errors.length} system errors while link checking:`);
+    sys_errors.forEach(e => console.error(`\t${e}`));
   }
 
   if (exit) {
     console.error(`${LC}: A FATAL ERROR occurred - exiting now...`);
     process.exit(1);
   }
+}
+
+function enumMap(cond, fmt, type) {
+  let list = [];
+  urlMap.forEach((v, k) => {
+    if (!cond(v)) return;
+    list[list.length] = fmt(v, k);
+  });
+  list.sort();
+  list.forEach(e => {
+    if (type === 'info') console.info(e);
+    else if (type === 'warn') console.warn(e);
+    else if (type === 'error') console.error(e);
+  });
 }
 
 module.exports = {
