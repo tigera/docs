0d273d1e98f16e8af7b7c2b4f3fd269874ad9640
Christopher Tauchen <chris.tauchen@tigera.io>
2023-01-27 17:07:15 +0000

    from CE pull # 5679: Adds docs for external networks

--- sidebars-calico-enterprise.js
+++ sidebars-calico-enterprise.js
@@ -173,6 +173,7 @@ module.exports = {
             'networking/egress/egress-gateway-on-prem',
             'networking/egress/egress-gateway-aws',
             'networking/egress/egress-gateway-maintenance',
+            'networking/egress/external-network',
             'networking/egress/troubleshoot',
           ],
         },
@@ -725,6 +726,7 @@ module.exports = {
               ]
             },
             'reference/resources/deeppacketinspection',
+            'reference/resources/externalnetwork',
             'reference/resources/felixconfig',
             'reference/resources/globalalert',
             'reference/resources/globalnetworkpolicy',
