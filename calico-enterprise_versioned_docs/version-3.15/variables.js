c801e26f6ec31bbb385681aad6b371bf536cb9ca
Christopher Tauchen <chris.tauchen@tigera.io>
2023-01-31 11:03:11 +0000

    Fixes CC codeblock with images

--- calico-enterprise_versioned_docs/version-3.15/variables.js
+++ calico-enterprise_versioned_docs/version-3.15/variables.js
@@ -1,12 +1,12 @@
 const releases = require('./releases.json');
 
 const variables = {
-  releaseTitle: 'v3.15.0',
+  releaseTitle: 'v3.15.1',
   prodname: 'Calico Enterprise',
   prodnamedash: 'calico-enterprise',
   version: 'v3.15',
   baseUrl: '/calico-enterprise/3.15',
-  filesUrl: 'https://downloads.tigera.io/ee/v3.15.0',
+  filesUrl: 'https://downloads.tigera.io/ee/v3.15.1',
   tutorialFilesURL: 'https://unified-docs.tigera.io/files',
   prodnameWindows: 'Calico Enterprise for Windows',
   downloadsurl: 'https://downloads.tigera.io',
