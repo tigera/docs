a40e34f468767e5b3d2d315fca1e0d36d0a62c0f
Christopher Tauchen <chris.tauchen@tigera.io>
2023-01-31 15:38:06 +0000

    Fix links on homepage

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -7,7 +7,7 @@ const FeatureList = [
   {
     title: 'Calico Open Source',
     img: '/img/calico-logo.webp',
-    link: 'calico/latest',
+    link: 'calico/',
     description: (
       <>
         Calico Open Source is a free, self-managed networking and security solution for containers, virtual machines,
@@ -19,7 +19,7 @@ const FeatureList = [
   {
     title: 'Calico Enterprise',
     img: '/img/calico-enterprise-logo.webp',
-    link: 'calico-enterprise/latest',
+    link: 'calico-enterprise/',
     description: (
       <>
         Calico Enterprise is a paid, self-managed security and observability solution for containers and Kubernetes.
