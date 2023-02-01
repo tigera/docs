f4c58b0a7c04aeca6f83604e828ce1cdfd0e7c2c
Doug Collier <doug@tigera.io>
2023-02-01 17:26:34 -0600

    update prod docs link checker

--- __tests__/manifest-urls.test.js
+++ __tests__/manifest-urls.test.js
@@ -1,7 +1,12 @@
 const { test, expect } = require('@playwright/test');
-const { PlaywrightCrawler, Dataset } = require('crawlee');
+const { PlaywrightCrawler, downloadListOfUrls } = require('crawlee');
 const linkChecker = require('../src/utils/linkChecker');
+
 const linkRegex = /https?:\/\/[-a-zA-Z0-9()@:%._+~#?&/=]+?\.(ya?ml|zip|ps1|tgz)/g;
+const DOCS = 'https://docs.tigera.io';
+const TESTER = /^https:\/\/docs\.tigera\.io/i;
+const SITEMAP = 'sitemap.xml';
+const SITEMAP_URL = `${DOCS}/${SITEMAP}`;
 
 test("Test file links to check if they're all reachable", async () => {
   const lc = linkChecker();
@@ -9,24 +14,43 @@ test("Test file links to check if they're all reachable", async () => {
   const crawler = new PlaywrightCrawler({
     // Use the requestHandler to process each of the crawled pages.
     async requestHandler({ request, page, enqueueLinks, log }) {
+      if (request.skipNavigation) return;
       const nodes = await page.getByText(lc.getLinkRegex());
       const allText = await nodes.allInnerTexts();
       for (const text of allText) {
         lc.process(text);
       }
       await enqueueLinks({
-        transformRequestFunction: request => {
-          if (linkRegex.test(request.url)) {
-            lc.process(request.url);
-            request.skipNavigation = true;
+        transformRequestFunction: (ro) => {
+          if (linkRegex.test(ro.url)) {
+            lc.process(ro.url);
+            ro.skipNavigation = true;
           }
-          return request;
+          return ro;
         }
       })
     },
   });
 
-  await crawler.run(['https://docs.tigera.io']);
+  const urlCache = new Map();
+
+  async function processSiteMap(siteMapUrl) {
+    const urls = await downloadListOfUrls({ url: siteMapUrl });
+    for (const url of urls) {
+      if (!TESTER.test(url)) continue;
+      if (urlCache.has(url)) continue;
+      urlCache.set(url, null);
+      if (url.endsWith(SITEMAP)) {
+        await processSiteMap(url);
+      }
+    }
+  }
+
+  await crawler.addRequests([DOCS]);
+  await processSiteMap(SITEMAP_URL);
+  const urls = [...urlCache.keys()].filter(url => !url.endsWith(SITEMAP));
+  await crawler.addRequests(urls);
+  await crawler.run();
   lc.report();
 
   // const allErrors = lc.sysErrorsCount() + lc.errorCount() + lc.deadCount();
