47a119efa3355a3481391ce521b4fd0252622199
Christopher Tauchen <chris.tauchen@tigera.io>
2023-02-10 14:03:31 +0000

    Stages CE v3.16.0 for publication

---
+++ calico-enterprise_versioned_docs/version-3.16/variables.js
@@ -0,0 +1,26 @@
+const releases = require('./releases.json');
+
+const variables = {
+  releaseTitle: 'v3.16.0',
+  prodname: 'Calico Enterprise',
+  prodnamedash: 'calico-enterprise',
+  version: 'v3.16',
+  filesUrl: 'https://downloads.tigera.io/ee/v3.16.0',
+  tutorialFilesURL: 'https://docs.tigera.io/files',
+  tmpScriptsURL: 'https://docs.tigera.io/calico-enterprise/3.16',
+  prodnameWindows: 'Calico Enterprise for Windows',
+  downloadsurl: 'https://downloads.tigera.io',
+  nodecontainer: 'cnx-node',
+  noderunning: 'calico-node',
+  rootDirWindows: 'C:\\TigeraCalico',
+  registry: 'gcr.io/unique-caldron-775/cnx/',
+  chart_version_name: 'v3.16.0-0',
+  tigeraOperator: releases[0]['tigera-operator'],
+  releases,
+  imageNames: {
+    node: 'tigera/cnx-node',
+    kubeControllers: 'tigera/kube-controllers',
+  },
+};
+
+module.exports = variables;
