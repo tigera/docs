6bd1924a28f64fc189592095f3e72d8df9cbf557
Doug Collier <doug@tigera.io>
2023-03-14 15:37:29 -0500

    add switch-latest script

---
+++ scripts/switch-latest.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+
+product=$1
+fromVersion=$2
+toVersion=$3
+
+function help() {
+  echo "This script requires 3 arguments: a product name (calico or calico-enterprise), a 'from' version, and a 'to' version"
+  echo "For example: ./script/switch-latest.sh calico 3.25 3.26"
+  exit 1
+}
+
+if [[ "${product}" != "calico" && "${product}" != "calico-enterprise" ]]; then
+  help
+elif [[ -z $fromVersion || -z $toVersion ]]; then
+  help
+fi
+
+echo 'Switching latest...'
+
+perl -0777 -pi -e "s/^[ \t]*baseUrl:[ \t]*'\/${product}/latest'[ \t]*,[ \t]*\$/m" "./${product}_versioned_docs/version-${fromVersion}/variables.js"
+perl -0777 -pi -e "s/^[ \t]*baseUrl:[ \t]*'\/${product}/${toVersion}'[ \t]*,[ \t]*\$/m" "./${product}_versioned_docs/version-${toVersion}/variables.js"
+perl -0777 -pi -e "s/label:[ \t]*'${fromVersion}'[ \t]*,[ \t]*\n([ \t]*)path:[ \t]*'latest'[ \t]*,/label: '${fromVersion}',\n\${1}path: '${fromVersion}',/" "./docusaurus.config.js"
+perl -0777 -pi -e "s/label:[ \t]*'${toVersion}'[ \t]*,[ \t]*\n([ \t]*)path:[ \t]*'latest'[ \t]*,/label: '${toVersion}',\n\${1}path: 'latest',/" "./docusaurus.config.js"
+
+echo "[SUCCESS]: version 'latest' for product '${product}' switched from ${fromVersion} to ${toVersion}"
