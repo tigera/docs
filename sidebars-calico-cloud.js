7039edceb7f3dbae91a08600e7fbbd60e26a1143
Brian McMahon <brianmcmahon135@gmail.com>
2023-03-29 16:19:56 -0700

    Add documentation for enabling / configuring the runtime view
    This PR separates out the details of the runtime view into it's own document, and does a more comprehensive overview of what it does, how you enable it, and how it needs to be configured in special cases.
    
    It outlines pre requisites around requiring k8s clusters that use CRI, and highlights what components are installed and what privileges they need.

--- sidebars-calico-cloud.js
+++ sidebars-calico-cloud.js
@@ -298,6 +298,7 @@ module.exports = {
       link: {type: 'doc', id: 'image-assurance/index'},
       items: [
         'image-assurance/scan-image-registries',
+        "image-assurance/configuring-the-runtime-view",
         'image-assurance/set-up-alerts',
         'image-assurance/install-the-admission-controller'
       ],
