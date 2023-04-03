dbab493800a5fc65e1b6dae786ec9d0cc39acfb5
Barbara McKercher <barbara@tigera.io>
2023-04-03 14:41:25 -0700

    Fix format error

--- calico-enterprise_versioned_docs/version-3.16/variables.js
+++ calico-enterprise_versioned_docs/version-3.16/variables.js
@@ -19,7 +19,7 @@ const variables = {
   tigeraOperator: releases[0]['tigera-operator'],
   manifestsUrl: 'https://docs.tigera.io/v3.16.1/',
   releases,
-  imageNames:
+  imageNames: {
     node: 'tigera/cnx-node',
     kubeControllers: 'tigera/kube-controllers',
   },
