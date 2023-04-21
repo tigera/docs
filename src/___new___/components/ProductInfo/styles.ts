7f96767f94af4387c8205371d4e00a1d0c91bdf1
Hugo <hugoj.l.ferreira@gmail.com>
2023-04-21 14:30:02 +0100

    alternate dark mode

--- src/___new___/components/ProductInfo/styles.ts
+++ src/___new___/components/ProductInfo/styles.ts
@@ -1,5 +1,6 @@
 export const prodContainerStyles = (hasDarkBg: boolean) => ({
-  bg: hasDarkBg ? 'tigeraBlack' : 'tigeraGrey.100',
+  //   bg: hasDarkBg ? 'tigeraBlack' : 'tigeraGrey.100',
+  bg: hasDarkBg ? 'linear-gradient(105.96deg, #0964AD 56.79%, #06547A 100%)' : 'tigeraGrey.100',
 
   h: '100%',
   w: '100%',
@@ -8,7 +9,8 @@ export const prodContainerStyles = (hasDarkBg: boolean) => ({
 
 export const sectionOuterStyles = (hasDarkBg: boolean) => ({
   w: '100%',
-  bg: hasDarkBg ? 'tigeraBlack' : 'tigeraGrey.100',
+  bg: hasDarkBg ? 'linear-gradient(105.96deg, #0964AD 56.79%, #06547A 100%)' : 'tigeraGrey.100',
+
   padding: '0 6rem',
 });
 
