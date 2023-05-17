970fa73c34f37db5efbda9e6740a7f56b607923f
Hugo <hugoj.l.ferreira@gmail.com>
2023-05-17 13:48:09 +0100

    Clym funcionality for docs.tigera.io

--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -203,19 +203,7 @@ function DocSearch({ contextualSearch, externalUrlRegex, ...props }) {
     searchButtonRef,
   });
 
-  (function (d, s, i, w, o) {
-    var js,
-        cjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(i)) return;
 
-    js = d.createElement('script');
-    js.id = i;
-    js.src = 'https://widget.clym-sdk.net/clym.js';
-    js.onload = function () {
-        Clym && Clym.load(i, w, o);
-    };
-    cjs.parentNode.insertBefore(js, cjs);
-})(document, 'script', 'clym-privacy', 'efccb797d8fa43acba85c4d5dutupsek', {});
 
   return (
     <>
