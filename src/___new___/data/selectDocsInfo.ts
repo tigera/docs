8c0cb8cf8b1d05a86708176ad05765f001cf5723
Barbara McKercher <barbara@tigera.io>
2023-05-30 10:23:55 -0700

    Fix-links-landing-page

--- src/___new___/data/selectDocsInfo.ts
+++ src/___new___/data/selectDocsInfo.ts
@@ -4,17 +4,17 @@ export default {
   selectDocsInfo: [
     {
       title: 'Calico Open Source',
-      link: '/calico/latest/about/',
+      link: '/calico/latest/about/about-calico',
       image: 'img/calico-logo.webp',
     },
     {
       title: 'Calico Enterprise',
-      link: '/calico-enterprise/latest/about-calico-enterprise',
+      link: '/calico-enterprise/latest/about/',
       image: '/img/calico-enterprise-logo.webp',
     },
     {
       title: 'Calico Cloud',
-      link: 'calico-cloud/about',
+      link: 'calico-cloud/about/',
       image: '/img/calico-cloud-logo.webp',
     },
   ],
