7d8e559d837fc0ed7caea4d1b34d002a8cb421b4
Onong Tayeng <onong.tayeng@gmail.com>
2023-06-21 12:14:19 +0530

    Fix the vppbranch var for v3.24, v3.25 and v3.26 branches

--- calico_versioned_docs/version-3.24/variables.js
+++ calico_versioned_docs/version-3.24/variables.js
@@ -18,7 +18,7 @@ const variables = {
   manifestsUrl: 'https://raw.githubusercontent.com/projectcalico/calico/v3.24.6',
   releases,
   registry: '',
-  vppbranch: 'v3.24.0',
+  vppbranch: 'v3.24.1',
   tigeraOperator: releases[0]['tigera-operator'],
   tigeraOperatorVersionShort: releases[0]['tigera-operator'].version.split('.').slice(0, 2).join('.'),
   imageNames: {
