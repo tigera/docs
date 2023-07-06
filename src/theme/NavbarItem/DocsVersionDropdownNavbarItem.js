6996d77bd80dc31396530a0f9eabd36f280bc728
Christopher Tauchen <chris.tauchen@tigera.io>
2023-07-06 10:32:41 +0100

    Adds configuration to build Calico Cloud versions for review

--- src/theme/NavbarItem/DocsVersionDropdownNavbarItem.js
+++ src/theme/NavbarItem/DocsVersionDropdownNavbarItem.js
@@ -5,9 +5,13 @@ import { useProductId } from '../../utils/useProductId';
 export default function DocsVersionDropdownNavbarItemWrapper(props) {
   const productId = useProductId();
 
+  {/* Commented out to allow navigation between Calico Cloud versions.
+
   if (!productId || productId === 'calico-cloud') {
     return null;
   }
+
+  */}
 
   const dropdownItemsAfter = props.dropdownItemsAfter.filter(
     (item) => !item.className || !item.className.startsWith('product') || item.className.endsWith(productId)
