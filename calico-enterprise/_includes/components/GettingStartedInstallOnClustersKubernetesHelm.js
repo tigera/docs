336a3ecfb81f40aa2330267d199dd93530c15511
afra <122131693+ti-afra@users.noreply.github.com>
2023-07-11 16:18:10 -0700

    [EV-3803] Add helm installation reference doc

--- calico-enterprise/_includes/components/GettingStartedInstallOnClustersKubernetesHelm.js
+++ calico-enterprise/_includes/components/GettingStartedInstallOnClustersKubernetesHelm.js
@@ -51,8 +51,9 @@ EOF`}
         </CodeBlock>
         <li>
           <p>
-            Add any other customizations you require to <code>values.yaml</code>. You might like to refer to the{' '}
-            <a href='https://helm.sh/docs/'>helm docs</a> or run:
+              Add any other customizations you require to <code>values.yaml</code> by running the following command.
+              For help, see <a href='../../../reference/installation/helm_customization'>Helm installation reference</a>,
+              or <a href='https://helm.sh/docs/'>helm docs</a>.
           </p>
         </li>
         {renderCond2()}
