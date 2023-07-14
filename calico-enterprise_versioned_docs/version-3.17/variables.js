a8bedd176ff7f0f8137f1dca6c99a031117bd1d3
Barbara McKercher <barbara@tigera.io>
2023-07-14 14:00:53 -0700

    DOCS-1638-CC-3.17.1

--- calico-enterprise_versioned_docs/version-3.17/variables.js
+++ calico-enterprise_versioned_docs/version-3.17/variables.js
@@ -5,7 +5,11 @@ const variables = {
   prodname: 'Calico Enterprise',
   prodnamedash: 'calico-enterprise',
   version: 'v3.17',
+<<<<<<< HEAD
   baseUrl: '/calico-enterprise/latest',
+=======
+  baseUrl: '/calico-enterprise/3.17',
+>>>>>>> ca381156 (Update docs for GA.)
   filesUrl: 'https://downloads.tigera.io/ee/v3.17.1',
   tutorialFilesURL: 'https://docs.tigera.io/files',
   tmpScriptsURL: 'https://docs.tigera.io/calico-enterprise/3.17',
