00a7ef356f663a1f4d78e675d6b4c618dc3e53af
Barbara McKercher <barbara@tigera.io>
2023-08-15 11:13:16 -0700

    DOCS-1688-3.15.4

--- calico-enterprise_versioned_docs/version-3.15/variables.js
+++ calico-enterprise_versioned_docs/version-3.15/variables.js
@@ -1,12 +1,12 @@
 const releases = require('./releases.json');
 
 const variables = {
-  releaseTitle: 'v3.15.3',
+  releaseTitle: 'v3.15.4',
   prodname: 'Calico Enterprise',
   prodnamedash: 'calico-enterprise',
   version: 'v3.15',
   baseUrl: '/calico-enterprise/3.15',
-  filesUrl: 'https://downloads.tigera.io/ee/v3.15.3',
+  filesUrl: 'https://downloads.tigera.io/ee/v3.15.4',
   tutorialFilesURL: 'https://docs.tigera.io/files',
   tmpScriptsURL: 'https://docs.tigera.io/calico-enterprise/3.15',
   prodnameWindows: 'Calico Enterprise for Windows',
@@ -15,7 +15,7 @@ const variables = {
   noderunning: 'calico-node',
   rootDirWindows: 'C:\\TigeraCalico',
   registry: 'quay.io/',
-  chart_version_name: 'v3.15.3-0',
+  chart_version_name: 'v3.15.4-0',
   tigeraOperator: releases[0]['tigera-operator'],
   manifestsUrl: 'https://docs.tigera.io/v3.15/',
   releases,
