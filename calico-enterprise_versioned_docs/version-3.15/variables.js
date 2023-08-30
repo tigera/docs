bfc1cf9c7c7cd62b864893c13536a7e7f6a5e02c
Christopher Tauchen <chris.tauchen@tigera.io>
2023-08-30 12:58:09 +0100

    Cleans up manifestsUrl variable

--- calico-enterprise_versioned_docs/version-3.15/variables.js
+++ calico-enterprise_versioned_docs/version-3.15/variables.js
@@ -17,7 +17,6 @@ const variables = {
   registry: 'quay.io/',
   chart_version_name: 'v3.15.3-0',
   tigeraOperator: releases[0]['tigera-operator'],
-  manifestsUrl: 'https://docs.tigera.io/v3.15/',
   releases,
   imageNames: {
     node: 'tigera/cnx-node',
