820fa8f6b08cee7620c98ae7ee224ff17feaa5a6
Christopher Tauchen <chris.tauchen@tigera.io>
2023-09-13 14:27:18 +0100

    DOCS-1776: Cuts docs for CC 18

---
+++ calico-cloud_versioned_docs/version-18/variables.js
@@ -0,0 +1,29 @@
+const releases = require('./releases.json');
+
+const variables = {
+  releaseTitle: 'v3.16.1',
+  prodname: 'Calico Cloud',
+  prodnamedash: 'calico-cloud',
+  baseUrl: '/calico-cloud',
+  filesUrl: 'https://docs.calicocloud.io',
+  filesUrl_CE: 'https://downloads.tigera.io/ee/v3.16.1',
+  tutorialFilesURL: 'https://docs.tigera.io/files',
+  prodnameWindows: 'Calico Enterprise for Windows',
+  rootDirWindows: 'C:\\TigeraCalico',
+  nodecontainer: 'cnx-node',
+  noderunning: 'calico-node',
+  cloudversion: 'v3.16.1-5',
+  clouddownloadurl: 'https://installer.calicocloud.io/manifests/v3.16.1-5',
+  clouddownloadbase: 'https://installer.calicocloud.io',
+  cloudoperatorimage: 'quay.io/tigera/cc-operator',
+  imageassuranceversion: 'v1.5.6',
+  tigeraOperator: releases[0]['tigera-operator'],
+  releases,
+  registry: 'quay.io/',
+  imageNames: {
+    node: 'tigera/cnx-node',
+    kubeControllers: 'tigera/kube-controllers',
+  },
+};
+
+module.exports = variables;
