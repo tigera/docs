7d537ef44da0fc998dab14b9be96cf79356acfd0
Christopher Tauchen <chris.tauchen@tigera.io>
2023-09-26 15:52:50 +0100

    DOCS-1700: Adds config for CC version next previews

---
+++ scripts/cc-next-preview-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script makes changes to docusuarus.config.js.
+# These changes make Docusuarus publish only version Next for each product doc set.
+
+sed -i "s/onBrokenLinks: 'throw'/onBrokenLinks: 'warn'/" docusaurus.config.js
+sed -i "s/onBrokenMarkdownLinks: 'throw'/onBrokenMarkdownLinks: 'warn'/" docusaurus.config.js
+sed -i "s/onlyIncludeVersions: \[.*\]/onlyIncludeVersions: \['current'\]/" docusaurus.config.js
+sed -i "/lastVersion/d" docusaurus.config.js
+
+# Changes links from landing page to version next
+sed -i "s/calico\/latest\/about/calico\/next\/about/" src/___new___/data/selectDocsInfo.ts
+sed -i "s/calico-enterprise\/latest\/about/calico-enterprise\/next\/about/" src/___new___/data/selectDocsInfo.ts
+sed -i "s/calico-cloud\/about/calico\/next\/about/" src/___new___/data/selectDocsInfo.ts
+
+yarn build
