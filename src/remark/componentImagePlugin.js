c3bb523c5140fd40bd4cbb02f63c0bf7f842dbd2
Christopher Tauchen <chris.tauchen@tigera.io>
2023-12-19 14:56:50 +0000

    Fix for componentImagePlugin

--- src/remark/componentImagePlugin.js
+++ src/remark/componentImagePlugin.js
@@ -3,7 +3,7 @@ import { visit } from 'unist-util-visit';
 import getVariableByFilePath from '../utils/getVariableByFilePath';
 import componentImage from '../components/utils/componentImage';
 
-const COMPONENT_IMAGE_REGEX = new RegExp(/%%\s+component_image\(["']([\w-]+)["']\)\s+%%, 'g');
+const COMPONENT_IMAGE_REGEX = new RegExp(/%%\s+component_image\(["']([\w-]+)["']\)\s+%%/, 'g');
 
 function componentImagePlugin(_options) {
   async function transformer(tree, file) {
