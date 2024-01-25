b8f7007e671b4e2cc7b3b6dc375bbfe4ed00486d
Barbara McKercher <barbara@tigera.io>
2024-01-25 10:59:20 -0800

    DOCS-publish-ce-3.19-ep1

--- calico-enterprise_versioned_docs/version-3.19-1/variables.js
+++ calico-enterprise_versioned_docs/version-3.19-1/variables.js
@@ -1,25 +1,22 @@
 const releases = require('./releases.json');
 
 const variables = {
-  releaseTitle: 'master',
+  releaseTitle: 'v3.19.0-1.0',
   prodname: 'Calico Enterprise',
   prodnamedash: 'calico-enterprise',
-  version: 'master',
-  baseUrl: '/calico-enterprise/next',
-  filesUrl: 'https://downloads.tigera.io/ee/master',
+  version: 'v3.19',
+  baseUrl: '/calico-enterprise/latest',
+  filesUrl: 'https://downloads.tigera.io/ee/v3.19.0-1.0',
   tutorialFilesURL: 'https://docs.tigera.io/files',
-  tmpScriptsURL: 'https://docs.tigera.io/calico-enterprise/next',
-  windowsScriptsURL: 'https://raw.githubusercontent.com/kubernetes-sigs/sig-windows-tools/master/hostprocess',
+  tmpScriptsURL: 'https://docs.tigera.io/calico-enterprise/3.19',
   prodnameWindows: 'Calico Enterprise for Windows',
-  prodnamedashWindows: 'calico-enterprise-for-windows',
   downloadsurl: 'https://downloads.tigera.io',
   nodecontainer: 'cnx-node',
   noderunning: 'calico-node',
   rootDirWindows: 'C:\\TigeraCalico',
-  registry: 'gcr.io/unique-caldron-775/cnx/', // Change to 'quay.io/' for new release
-  chart_version_name: 'master',
+  registry: 'quay.io/',
+  chart_version_name: 'v3.19.0-1.0',
   tigeraOperator: releases[0]['tigera-operator'],
-  manifestsUrl: 'https://docs.tigera.io/master',
   releases,
   imageNames: {
     node: 'tigera/cnx-node',
