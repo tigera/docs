edf9e8163c2c908a31c6327d917b3dc78e459094
Christopher Tauchen <chris.tauchen@tigera.io>
2024-02-26 17:06:39 +0000

    Updates images in vNext

--- calico-cloud/variables.js
+++ calico-cloud/variables.js
@@ -1,19 +1,19 @@
 const releases = require('./releases.json');
 
 const variables = {
-  releaseTitle: 'v3.18.0-2.0',
+  releaseTitle: 'v3.19.0-1.0',
   prodname: 'Calico Cloud',
   prodnamedash: 'calico-cloud',
   baseUrl: '/calico-cloud',
   filesUrl: 'https://docs.calicocloud.io',
-  filesUrl_CE: 'https://downloads.tigera.io/ee/v3.18.0-2.0',
+  filesUrl_CE: 'https://downloads.tigera.io/ee/v3.19.0-1.0',
   tutorialFilesURL: 'https://docs.tigera.io/files',
   prodnameWindows: 'Calico Enterprise for Windows',
   rootDirWindows: 'C:\\TigeraCalico',
   nodecontainer: 'cnx-node',
   noderunning: 'calico-node',
-  cloudversion: 'v3.18.0-2.0-8',
-  clouddownloadurl: 'https://installer.calicocloud.io/manifests/v3.18.0-2.0-8',
+  cloudversion: 'v3.19.0-1.0-4',
+  clouddownloadurl: 'https://installer.calicocloud.io/manifests/v3.19.0-1.0-4',
   clouddownloadbase: 'https://installer.calicocloud.io',
   cloudoperatorimage: 'quay.io/tigera/cc-operator',
   imageassuranceversion: 'v1.13.0',
