e3a5cba7c403223e256c4f9eeb7ef1e139926184
Christopher Tauchen <chris.tauchen@tigera.io>
2024-03-04 15:33:34 +0000

    Upgrade to Docusaurus 3.1.1

---
+++ src/theme/footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from '@theme-original/Footer'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import { MendableFloatingButton } from '@mendable/search'
+
+export default function FooterWrapper(props) {
+  const {
+    siteConfig: { customFields },
+  } = useDocusaurusContext()
+
+  return (
+    <>
+      <MendableFloatingButton anon_key={customFields.mendableAnonKey} />
+      <Footer {...props} />
+    </>
+  )
+}
