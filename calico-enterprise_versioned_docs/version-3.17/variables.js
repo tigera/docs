238d7c92c95232ce90008f213c2530863f414c3f
Christopher Tauchen <chris.tauchen@tigera.io>
2024-03-12 14:51:11 +0000

    DOCS-2128: Initial changes for patch CE 3.17.4

--- calico-enterprise_versioned_docs/version-3.17/variables.js
+++ calico-enterprise_versioned_docs/version-3.17/variables.js
@@ -1,12 +1,12 @@
 const releases = require('./releases.json');
 
 const variables = {
-  releaseTitle: 'v3.17.3',
+  releaseTitle: 'v3.17.4',
   prodname: 'Calico Enterprise',
   prodnamedash: 'calico-enterprise',
   version: 'v3.17',
   baseUrl: '/calico-enterprise/3.17',
-  filesUrl: 'https://downloads.tigera.io/ee/v3.17.3',
+  filesUrl: 'https://downloads.tigera.io/ee/v3.17.4',
   tutorialFilesURL: 'https://docs.tigera.io/files',
   tmpScriptsURL: 'https://docs.tigera.io/calico-enterprise/3.17',
   prodnameWindows: 'Calico Enterprise for Windows',
@@ -15,7 +15,7 @@ const variables = {
   noderunning: 'calico-node',
   rootDirWindows: 'C:\\TigeraCalico',
   registry: 'quay.io/',
-  chart_version_name: 'v3.17.3-0',
+  chart_version_name: 'v3.17.4-0',
   tigeraOperator: releases[0]['tigera-operator'],
   releases,
   imageNames: {
