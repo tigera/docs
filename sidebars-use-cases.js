2035b41c0837f37b75a6a21b7a981f19c109d73a
Christopher Tauchen <chris.tauchen@tigera.io>
2024-04-12 12:32:56 +0100

    Adds config for use cases section

---
+++ sidebars-use-cases.js
@@ -0,0 +1,13 @@
+module.exports = {
+  useCaseSidebar: [
+    {
+      type: 'category',
+      label: 'Use cases',
+      collapsible: false,
+      collapsed: false,
+      items: [
+        'microsegmentation',
+      ],
+    },
+  ],
+}
