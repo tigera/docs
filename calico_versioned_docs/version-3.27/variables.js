63e0c6d5c3e9f7d7fcb67dd8f242222590a16dbe
Christopher Tauchen <chris.tauchen@tigera.io>
2024-05-01 12:18:55 +0100

    RN updates for OSS 3.28.0

--- calico_versioned_docs/version-3.27/variables.js
+++ calico_versioned_docs/version-3.27/variables.js
@@ -5,7 +5,7 @@ const variables = {
   prodname: 'Calico',
   prodnamedash: 'calico',
   version: 'v3.27',
-  baseUrl: '/calico/latest',
+  baseUrl: '/calico/3.27',
   filesUrl: 'https://projectcalico.docs.tigera.io/v3.27',
   tutorialFilesURL: 'https://docs.tigera.io/files',
   calicoReleasesURL: 'https://github.com/projectcalico/calico/releases/download',
