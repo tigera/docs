94d6763a1ecb0deb153278afeaeb7e69b1fe2481
Ronan Conneely <ronan.conneely@tigera.io>
2024-05-08 09:02:45 +0100

    docs summary page v1

---
+++ src/___new___/components/DocCardLink/styles.ts
@@ -0,0 +1,38 @@
+export const linkStyles = { textDecoration: 'none' };
+
+export const cardStyles = {
+  p: 4,
+  h: 'full',
+  _hover: {
+    boxShadow: 'rgb(0, 0, 0, 0.2) 2px 2px 8px',
+  },
+  transition: 'box-shadow 0.2s ease-in-out',
+  boxShadow: 'none',
+  borderWidth: '1px',
+  borderStyle: 'solid',
+  borderColor: 'tigeraGrey.200',
+};
+
+export const cardHeaderStyles = {
+  p: 0,
+  mb: 3,
+};
+
+export const headingStyles = {
+  fontSize: 'md',
+  m: 0,
+  color: 'tigeraBlack',
+  transition: 'color 0.2s ease-in-out',
+  _hover: {
+    color: 'tigeraBlueDark',
+  },
+};
+
+export const cardBodyStyles = {
+  p: 0,
+};
+
+export const cardTextStyles = {
+  fontSize: 'sm',
+  m: '0',
+};
