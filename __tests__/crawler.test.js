908b9469326999522c529cc2ae0134c1a60f32e7
Christopher Tauchen <chris.tauchen@tigera.io>
2024-05-16 20:38:20 +0100

    Added bad link to skip list

--- __tests__/crawler.test.js
+++ __tests__/crawler.test.js
@@ -162,7 +162,8 @@ test("Crawl the docs and execute tests", async () => {
     'https://apt.kubernetes.io/',
     'https://docs.tigera.io/calico-enterprise/3.15/about-calico-enterprise',
     'https://your-jira-instance-name.atlassian.net/rest/api/2/issue/',
-    'https://bugzilla.netfilter.org/show_bug.cgi?id=1119'
+    'https://bugzilla.netfilter.org/show_bug.cgi?id=1119',
+    'https://webexapis.com/v1/messages'
   ];
 
   const lc = linkChecker();
