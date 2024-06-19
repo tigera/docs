43f7dcaae62ba4b603d5310b585470fdbbaddd21
sujeet-kr <sujeet.kr@hotmail.com>
2024-06-19 14:34:23 -0400

    add id to the code-block for openshift manifests install instruction

--- calico_versioned_docs/version-3.27/_includes/components/InstallOpenShiftManifests.js
+++ calico_versioned_docs/version-3.27/_includes/components/InstallOpenShiftManifests.js
@@ -8,7 +8,7 @@ export default function InstallOpenShiftManifests() {
   return (
     <>
       <p>Download the {prodname} manifests for OpenShift and add them to the generated manifests directory:</p>
-      <CodeBlock language='bash'>
+      <CodeBlock id='installOCPManifests' language='bash'>
         {`mkdir calico
 wget -qO- ${calicoReleasesURL}/${releaseTitle}/ocp.tgz | tar xvz --strip-components=1 -C calico
 cp calico/* manifests/`}
