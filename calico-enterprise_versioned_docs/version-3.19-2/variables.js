d08e9cb2c34901cd879849976e085e136a367629
Christopher Tauchen <chris.tauchen@tigera.io>
2024-07-25 20:30:40 +0100

    DOCS-2266 CE 3.19.1 operator-only release (#1569)

--- calico-enterprise_versioned_docs/version-3.19-2/variables.js
+++ calico-enterprise_versioned_docs/version-3.19-2/variables.js
@@ -16,7 +16,7 @@ const variables = {
   noderunning: 'calico-node',
   rootDirWindows: 'C:\\TigeraCalico',
   registry: 'quay.io/',
-  chart_version_name: 'v3.19.1-0',
+  chart_version_name: 'v3.19.1-1',
   tigeraOperator: releases[0]['tigera-operator'],
   releases,
   imageNames: {
