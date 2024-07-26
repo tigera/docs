0c0f5d8adb7b78ac9396a1f5771606c82a8ac37f
Christopher Tauchen <chris.tauchen@tigera.io>
2024-07-26 17:18:55 +0100

    DOCS-2261: Version cutting for 3.20 EP1

---
+++ calico-enterprise_versioned_docs/version-3.20-1/variables.js
@@ -0,0 +1,28 @@
+const releases = require('./releases.json');
+
+const variables = {
+  releaseTitle: 'v3.20.0-1.0',
+  prodname: 'Calico Enterprise',
+  prodnamedash: 'calico-enterprise',
+  version: 'v3.20',
+  baseUrl: '/calico-enterprise/3.20',
+  filesUrl: 'https://downloads.tigera.io/ee/v3.20.0-1.0',
+  tutorialFilesURL: 'https://docs.tigera.io/files',
+  tmpScriptsURL: 'https://docs.tigera.io/calico-enterprise/3.20',
+  windowsScriptsURL: 'https://raw.githubusercontent.com/kubernetes-sigs/sig-windows-tools/master/hostprocess',
+  prodnameWindows: 'Calico Enterprise for Windows',
+  downloadsurl: 'https://downloads.tigera.io',
+  nodecontainer: 'cnx-node',
+  noderunning: 'calico-node',
+  rootDirWindows: 'C:\\TigeraCalico',
+  registry: 'quay.io/',
+  chart_version_name: 'v3.20.0-1.0-0',
+  tigeraOperator: releases[0]['tigera-operator'],
+  releases,
+  imageNames: {
+    node: 'tigera/cnx-node',
+    kubeControllers: 'tigera/kube-controllers',
+  },
+};
+
+module.exports = variables;
