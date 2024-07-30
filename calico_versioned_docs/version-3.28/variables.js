7efcb2271c9c8a2c60779e1618a021931bc7cd3b
Christopher Tauchen <chris.tauchen@tigera.io>
2024-07-30 17:23:03 +0100

    DOCS-2222: Prep OSS 3.28.1 release

--- calico_versioned_docs/version-3.28/variables.js
+++ calico_versioned_docs/version-3.28/variables.js
@@ -1,7 +1,7 @@
 const releases = require('./releases.json');
 
 const variables = {
-  releaseTitle: 'v3.28.0',
+  releaseTitle: 'v3.28.1',
   prodname: 'Calico',
   prodnamedash: 'calico',
   version: 'v3.28',
@@ -17,7 +17,7 @@ const variables = {
   noderunning: 'calico-node',
   rootDirWindows: 'C:\\CalicoWindows',
   ppa_repo_name: 'calico-3.28',
-  manifestsUrl: 'https://raw.githubusercontent.com/projectcalico/calico/v3.28.0',
+  manifestsUrl: 'https://raw.githubusercontent.com/projectcalico/calico/v3.28.1',
   releases,
   registry: '',
   vppbranch: 'v3.28.0',
