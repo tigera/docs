ac1e48ca51d4c0555e905b8bf0c41944287880ef
Christopher Tauchen <chris.tauchen@tigera.io>
2024-08-14 13:59:21 +0100

    DOCS-2298: Adds noindex meta to all category-level index pages.

---
+++ src/___new___/components/NoIndex/NoIndex.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Head from '@docusaurus/Head';
+
+const NoIndex = () => (
+  <Head>
+    <meta name="robots" content="noindex" />
+  </Head>
+);
+
+export default NoIndex;
