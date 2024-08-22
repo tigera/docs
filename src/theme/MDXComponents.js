6abaf6f8f12c68a922dd072ed075c949149492d9
Christopher Tauchen <chris.tauchen@tigera.io>
2024-08-22 12:10:55 +0100

    Remove versioned components and code from MDXComponents.js

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,107 +1,10 @@
 import React from 'react';
-
-import { useLocation } from '@docusaurus/router';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import MDXComponents from '@theme-original/MDXComponents';
-
 import GeekDetails from '@site/src/components/partials/GeekDetails';
 import Callouts from '@site/src/components/Callouts';
 
-// TO REGISTER A NEW COMPONENT
-//
-// Add component name to 'partials' array.
-// Add corresponding case to swith-case in getCalicoVersionedComponent(),
-// if component exists only for Calico, otherwise add a case for each
-// get{product}VersionedComponent(), if the product uses this component.
-
-const partials = [
-];
-
-const wrappedPartials = wrapPartials(partials);
-
 export default {
   ...MDXComponents,
-  ...wrappedPartials,
   GeekDetails,
   Callouts,
 };
-
-function resolveComponent(componentName) {
-  return (props) => {
-    const { pathname } = useLocation();
-    const context = useDocusaurusContext();
-
-    const [, prodnamedash, maybeVersion] = pathname.match(/\/(.*?)\/(.*?)\//);
-    const versions = context.globalData['docusaurus-plugin-content-docs'][prodnamedash].versions;
-
-    let Component = null;
-
-    const isNext = maybeVersion === 'next';
-    const noVersions = !versions || !versions.length;
-    if (isNext || noVersions) {
-      Component = getComponentForNextVersion(prodnamedash, componentName);
-    } else {
-      const isLatest = !versions.some((v) => v.name === maybeVersion);
-      const version = isLatest ? versions.find((v) => v.isLast).name : maybeVersion;
-
-      Component = getComponentForVersion(prodnamedash, componentName, version);
-    }
-
-    if (!Component) {
-      console.error(`Cannot resolve ${componentName} component for ${prodnamedash}`);
-    }
-
-    return <Component {...props} />;
-  };
-}
-
-function getComponentForNextVersion(prodnamedash, componentName) {
-  switch (prodnamedash) {
-    case 'calico-enterprise':
-      return require(`../../calico-enterprise/_includes/components/${componentName}`).default;
-    case 'calico':
-      return require(`../../calico/_includes/components/${componentName}`).default;
-    case 'calico-cloud':
-      return require(`../../calico-cloud/_includes/components/${componentName}`).default;
-    default:
-      console.error(`${prodnamedash} product doesn't exist`);
-  }
-}
-
-function getComponentForVersion(prodnamedash, componentName, version) {
-  switch (prodnamedash) {
-    case 'calico-enterprise':
-      return getCalicoEnterpriseVersionedComponent(version, componentName);
-    case 'calico':
-      return getCalicoVersionedComponent(version, componentName);
-    case 'calico-cloud':
-      return getCalicoCloudVersionedComponent(version, componentName);
-    default:
-      console.error(`${prodnamedash} product doesn't exist`);
-  }
-}
-
-function getCalicoVersionedComponent(version, componentName) {
-  switch (componentName) {
-    default:
-      console.error(`Versioned ${componentName} component isn't registered for Calico`);
-  }
-}
-
-function getCalicoCloudVersionedComponent(version, componentName) {
-  switch (componentName) {
-    default:
-      console.error(`Versioned ${componentName} component isn't registered for Calico Cloud`);
-  }
-}
-
-function getCalicoEnterpriseVersionedComponent(version, componentName) {
-  switch (componentName) {
-    default:
-      console.error(`Versioned ${componentName} component isn't registered for Calico Enterprise`);
-  }
-}
-
-function wrapPartials(partials) {
-  return Object.fromEntries(partials.map((name) => [name, resolveComponent(name)]));
-}
