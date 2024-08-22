69efd1b91f3ea5f6472772c2edcde2ef866cba6a
Christopher Tauchen <chris.tauchen@tigera.io>
2024-08-22 10:44:19 +0100

    Undo global component: InstallGKE

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -15,7 +15,6 @@ import Callouts from '@site/src/components/Callouts';
 // get{product}VersionedComponent(), if the product uses this component.
 
 const partials = [
-  'InstallGKE',
   'UpgradeOperatorSimple',
   'InstallOpenShift',
   'InstallOpenShiftManifests',
@@ -104,9 +103,6 @@ function getCalicoCloudVersionedComponent(version, componentName) {
 
 function getCalicoEnterpriseVersionedComponent(version, componentName) {
   switch (componentName) {
-    case 'InstallGKE':
-      return require(`../../calico-enterprise_versioned_docs/version-${version}/_includes/components/InstallGKE`)
-        .default;
     case 'InstallOpenShift':
       return require(`../../calico-enterprise_versioned_docs/version-${version}/_includes/components/InstallOpenShift`)
         .default;
