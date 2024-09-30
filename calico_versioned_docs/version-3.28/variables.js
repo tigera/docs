5688633e76af1215ef38c7828b72d6b31813054b
Christopher Tauchen <chris.tauchen@tigera.io>
2024-09-30 11:04:57 +0100

    Fixes VPP variable issue

--- calico_versioned_docs/version-3.28/variables.js
+++ calico_versioned_docs/version-3.28/variables.js
@@ -21,6 +21,7 @@ const variables = {
   releases,
   registry: '',
   vppbranch: 'v3.28.0',
+  vppFilesURL: 'https://raw.githubusercontent.com/projectcalico/vpp-dataplane/v3.28.0',
   tigeraOperator: releases[0]['tigera-operator'],
   tigeraOperatorVersionShort: releases[0]['tigera-operator'].version.split('.').slice(0, 2).join('.'),
   imageNames: {
