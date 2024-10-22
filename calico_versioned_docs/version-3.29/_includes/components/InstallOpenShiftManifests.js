8bbee5906654a271e72de65a2a986e0ac1a7826e
Christopher Tauchen <chris.tauchen@tigera.io>
2024-10-22 16:21:53 +0100

    Adds OSS v3.29

---
+++ calico_versioned_docs/version-3.29/_includes/components/InstallOpenShiftManifests.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import CodeBlock from '@theme/CodeBlock';
+
+import { prodname, releaseTitle, calicoReleasesURL } from '../../variables';
+
+export default function InstallOpenShiftManifests() {
+  return (
+    <>
+      <p>Download the {prodname} manifests for OpenShift and add them to the generated manifests directory:</p>
+      <CodeBlock id='data-install-openshift-manifests' language='bash'>
+        {`mkdir calico
+wget -qO- ${calicoReleasesURL}/${releaseTitle}/ocp.tgz | tar xvz --strip-components=1 -C calico
+cp calico/* manifests/`}
+      </CodeBlock>
+    </>
+  );
+}
