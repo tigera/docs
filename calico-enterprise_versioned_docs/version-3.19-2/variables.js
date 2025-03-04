388dcafdfb2ed0fb3a4263624faf286c5af6967a
Christopher Tauchen <chris.tauchen@tigera.io>
2025-03-04 19:40:22 +0000

    DOCS-2406: Publish Calico Enterprise 3.19.5 (#1928)
    Co-authored-by: tuti <tuti@tigera.io>

--- calico-enterprise_versioned_docs/version-3.19-2/variables.js
+++ calico-enterprise_versioned_docs/version-3.19-2/variables.js
@@ -2,12 +2,12 @@ const releases = require('./releases.json');
 const componentImage = require('../../src/components/utils/componentImage');
 
 const variables = {
-  releaseTitle: 'v3.19.4',
+  releaseTitle: 'v3.19.5',
   prodname: 'Calico Enterprise',
   prodnamedash: 'calico-enterprise',
   version: 'v3.19',
   baseUrl: '/calico-enterprise/3.19',
-  filesUrl: 'https://downloads.tigera.io/ee/v3.19.4',
+  filesUrl: 'https://downloads.tigera.io/ee/v3.19.5',
   tutorialFilesURL: 'https://docs.tigera.io/files',
   tmpScriptsURL: 'https://docs.tigera.io/calico-enterprise/3.19',
   windowsScriptsURL: 'https://raw.githubusercontent.com/kubernetes-sigs/sig-windows-tools/master/hostprocess',
@@ -17,7 +17,7 @@ const variables = {
   noderunning: 'calico-node',
   rootDirWindows: 'C:\\TigeraCalico',
   registry: 'quay.io/',
-  chart_version_name: 'v3.19.4-0',
+  chart_version_name: 'v3.19.5-0',
   tigeraOperator: releases[0]['tigera-operator'],
   dikastesVersion: releases[0].components.dikastes.version,
   releases,
@@ -27,7 +27,7 @@ const variables = {
   },
   componentImage: {
     cnxNode: componentImage('cnx-node', releases[0]),
-    calicoctl:componentImage('calicoctl', releases[0]), 
+    calicoctl: componentImage('calicoctl', releases[0]),
     calicoq: componentImage('calicoq', releases[0]),
   },
 };
