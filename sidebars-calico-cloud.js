619b517e31fbb099e4a5480d7f0be962cf05bdf8
Christopher Tauchen <chris.tauchen@tigera.io>
2025-03-31 15:20:12 +0100

    Removes Admission Controller feature

--- sidebars-calico-cloud.js
+++ sidebars-calico-cloud.js
@@ -286,7 +286,6 @@ module.exports = {
         'image-assurance/understanding-scan-results',
         'image-assurance/exclude-vulnerabilities-from-scan-results',
         'image-assurance/set-up-alerts',
-        'image-assurance/install-the-admission-controller',
         'image-assurance/creating-jira-issues-for-scan-results',
       ],
     },
