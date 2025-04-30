94f897d4ed3f16a8cc63b1308bb04703295dba53
Christopher Tauchen <chris.tauchen@tigera.io>
2025-04-30 15:34:48 +0100

    Modify DocVersionBadge to display Free and Pro

--- src/theme/DocVersionBadge/index.js
+++ src/theme/DocVersionBadge/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import clsx from 'clsx';
-import Translate from '@docusaurus/Translate';
 import {ThemeClassNames} from '@docusaurus/theme-common';
 import {useDocsVersion} from '@docusaurus/plugin-content-docs/client';
 import {useActivePlugin} from '@docusaurus/plugin-content-docs/client';
+import { useLocation } from '@docusaurus/router';
 import styles from './styles.module.css';
 
 export default function DocVersionBadge({className}) {
   const versionMetadata = useDocsVersion();
   const activePlugin = useActivePlugin();
+  const location = useLocation();
   const pluginId = activePlugin?.pluginId || 'unknown-plugin';
+  const currentPath = location.pathname;
 
   const pluginNameMap = {
     'calico': 'Calico Open Source',
@@ -23,6 +25,25 @@ export default function DocVersionBadge({className}) {
     return null;
   }
 
+  let badgeText;
+
+  if (pluginId === 'calico-cloud') {
+    const isFreeTier = currentPath.includes('/calico-cloud/free/');
+    badgeText = isFreeTier
+      ? 'Calico Cloud Free documentation'
+      : 'Calico Cloud Pro documentation';
+  }
+
+  else {
+    badgeText = versionMetadata.badge
+      ? `${pluginName} ${versionMetadata.label} documentation`
+      : `${pluginName} documentation`;
+  }
+
+  if (!badgeText) {
+      return null;
+  }
+
   return (
       <span
         className={clsx(
@@ -30,16 +51,7 @@ export default function DocVersionBadge({className}) {
           ThemeClassNames.docs.docVersionBadge, styles.hcu,
           'badge badge--secondary',
         )}>
-        <Translate
-          id="theme.docs.versionBadge.label"
-          values={{
-            versionLabel: versionMetadata.label,
-            pluginName: pluginName,
-          }}>
-        {versionMetadata.badge
-          ? "{pluginName} {versionLabel} documentation"
-          : "{pluginName} documentation"}
-        </Translate>
+        {badgeText}
       </span>
     );
 }
