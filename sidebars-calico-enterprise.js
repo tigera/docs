c72767e2573b8a37fefed5e990d52db0a79541fe
Pedro Coutinho <coutinhop@users.noreply.github.com>
2025-05-01 01:20:03 -0700

    [CORE-10980] Add documentation for QoS Controls (#1971)

--- sidebars-calico-enterprise.js
+++ sidebars-calico-enterprise.js
@@ -176,6 +176,7 @@ module.exports = {
             'networking/configuring/workloads-outside-cluster',
             'networking/configuring/pod-mac-address',
             'networking/configuring/node-local-dns-cache',
+            'networking/configuring/qos-controls',
           ],
         },
         {
