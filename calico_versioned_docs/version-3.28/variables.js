8606dded6b481ec1b4365be4f391d4ba36d4ba95
Christopher Tauchen <chris.tauchen@tigera.io>
2025-06-11 14:01:29 +0100

    Prepare for patch release OSS 3.28.5

--- calico_versioned_docs/version-3.28/variables.js
+++ calico_versioned_docs/version-3.28/variables.js
@@ -1,7 +1,7 @@
 const releases = require('./releases.json');
 
 const variables = {
-  releaseTitle: 'v3.28.4',
+  releaseTitle: 'v3.28.5',
   prodname: 'Calico',
   prodnamedash: 'calico',
   version: 'v3.28',
@@ -17,7 +17,7 @@ const variables = {
   noderunning: 'calico-node',
   rootDirWindows: 'C:\\CalicoWindows',
   ppa_repo_name: 'calico-3.28',
-  manifestsUrl: 'https://raw.githubusercontent.com/projectcalico/calico/v3.28.4',
+  manifestsUrl: 'https://raw.githubusercontent.com/projectcalico/calico/v3.28.5',
   releases,
   registry: '',
   vppbranch: 'v3.28.0',
