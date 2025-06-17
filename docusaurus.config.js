74d6c0ba80de7a72cbfd4230bfd1ea971cbf682d
Christopher Tauchen <chris.tauchen@tigera.io>
2025-06-17 12:55:28 +0100

    Cut version CC 21-2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -469,6 +469,10 @@ export default async function createAsyncConfig() {
               path: 'next',
               banner: 'unreleased',
             },
+            '21-2': {
+              path: '/',
+              banner: 'none',
+            },
             '21-1': {
               path: '/',
               banner: 'none'
