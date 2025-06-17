d3c55b4e8f269bdcf7c005cd55fac8ddcfd14300
Christopher Tauchen <chris.tauchen@tigera.io>
2025-06-17 12:50:40 +0100

    Fix cut-new-version regex
    The regex takes imported componetns from the versioned _imports
    directory and changes them to the new version.
    
    It was importing only MDX files, and not other versioned imports.
    
    This PR fixes the regex to include all versioned import lines.

--- scripts/cut-new-version.sh
+++ scripts/cut-new-version.sh
@@ -8,7 +8,7 @@ echo 'Cutting a new version...'
 yarn docusaurus docs:version:$product $version
 
 echo 'Updating MDX imports...'
-regex="s/import (.*) from\s* '\@site\/${product}\/(.*)\.mdx';/import \${1} from '\@site\/${product}_versioned_docs\/version-${version}\/\${2}.mdx';/g"
+regex="s/import (.*) from\s* '\@site\/${product}\/(.*)';/import \${1} from '\@site\/${product}_versioned_docs\/version-${version}\/\${2}';/g"
 find "./${product}_versioned_docs/version-${version}" -path '*/*.mdx' -type f -print0 | xargs -0 perl -0777 -pi -e "${regex}"
 echo '[SUCCESS]: imports updated!'
 echo
