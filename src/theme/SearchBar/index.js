eb85edce7e4c098d43980659818cf5b052628a55
Ronan Conneely <139970314+ronanc-tigera@users.noreply.github.com>
2025-06-30 11:07:50 +0100

    [DOCS#2618] search bar kapa ai (#2137)

--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -12,6 +12,9 @@ import Translate from '@docusaurus/Translate';
 import translations from '@theme/SearchTranslations';
 import { useProductId } from '../../utils/useProductId';
 import { getProductNameById } from '../../utils/getProductNameById';
+import { Flex, Button, Center, Box } from '@chakra-ui/react';
+import { SearchModalAskAI } from '../../___new___/components';
+import useIsBrowser from '@docusaurus/useIsBrowser';
 
 const getUrlParams = ({query, productId, preferredVersion}) => {
   const params = new URLSearchParams();
@@ -107,7 +110,6 @@ function DocSearch({ contextualSearch, externalUrlRegex, ...props }) {
       ...props.searchParameters,
       facetFilters: productId ? filterFacetFiltersByProduct(facetFilters, productId) : facetFilters,
     });
-    console.log('searchParameters',searchParameters)
   }, [productId]);
   const [footer, setFooter] = useState();
   const { withBaseUrl } = useBaseUrlUtils();
@@ -206,7 +208,12 @@ function DocSearch({ contextualSearch, externalUrlRegex, ...props }) {
     searchButtonRef,
   });
 
+  const isBrowser = useIsBrowser();
+  let searchBar;
 
+  if (isBrowser) {
+    searchBar = document.querySelector('.DocSearch-SearchBar')
+  }
 
   return (
     <>
@@ -273,6 +280,11 @@ function DocSearch({ contextualSearch, externalUrlRegex, ...props }) {
             />
           </div>,
           searchContainer.current
+        )}
+
+        {searchBar && isOpen && createPortal(
+          <SearchModalAskAI onClose={() => setIsOpen(false)} />,
+          searchBar
         )}
     </>
   );
