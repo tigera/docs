78bc9feac0c74c390a35f29592cbf9da96f22860
Onong Tayeng <onong.tayeng@gmail.com>
2025-07-11 07:40:44 +0530

    Update vppbranch var for Calico/VPP v3.30.0

--- calico_versioned_docs/version-3.30/variables.js
+++ calico_versioned_docs/version-3.30/variables.js
@@ -20,7 +20,7 @@ const variables = {
   manifestsUrl: 'https://raw.githubusercontent.com/projectcalico/calico/v3.30.3',
   releases,
   registry: '',
-  vppbranch: 'v3.29.0',
+  vppbranch: 'v3.30.0',
   tigeraOperator: releases[0]['tigera-operator'],
   tigeraOperatorVersionShort: releases[0]['tigera-operator'].version.split('.').slice(0, 2).join('.'),
   imageNames: {
