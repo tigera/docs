cb00cb582bd45763cc125414c6274b8dc9e48167
Christopher Tauchen <chris.tauchen@tigera.io>
2025-07-23 12:21:18 +0100

    Cut version: Calico Enterprise 3.22-1

---
+++ calico-enterprise_versioned_docs/version-3.22-1/variables.js
@@ -0,0 +1,39 @@
+const releases = require('./releases.json');
+const componentImage = require('../src/components/utils/componentImage');
+
+const variables = {
+  releaseTitle: 'master',
+  prodname: 'Calico Enterprise',
+  prodnamedash: 'calico-enterprise',
+  version: 'master',
+  openSourceVersion: releases[0].calico.minor_version.slice(1),
+  baseUrl: '/calico-enterprise/next',
+  filesUrl: 'https://downloads.tigera.io/ee/master',
+  rpmsUrl: 'https://downloads.tigera.io/ee/rpms/v3.21',
+  tutorialFilesURL: 'https://docs.tigera.io/files',
+  tmpScriptsURL: 'https://docs.tigera.io/calico-enterprise/next',
+  windowsScriptsURL: 'https://raw.githubusercontent.com/kubernetes-sigs/sig-windows-tools/master/hostprocess',
+  prodnameWindows: 'Calico Enterprise for Windows',
+  prodnamedashWindows: 'calico-enterprise-for-windows',
+  downloadsurl: 'https://downloads.tigera.io',
+  nodecontainer: 'cnx-node',
+  noderunning: 'calico-node',
+  rootDirWindows: 'C:\\TigeraCalico',
+  registry: 'gcr.io/unique-caldron-775/cnx/', // Change to 'quay.io/' for new release
+  chart_version_name: 'master',
+  tigeraOperator: releases[0]['tigera-operator'],
+  dikastesVersion: releases[0].components.dikastes.version,
+  manifestsUrl: 'https://docs.tigera.io/master',
+  releases,
+  imageNames: {
+    node: 'tigera/cnx-node',
+    kubeControllers: 'tigera/kube-controllers',
+  },
+  componentImage: {
+    cnxNode: componentImage('cnx-node', releases[0]),
+    calicoctl:componentImage('calicoctl', releases[0]), 
+    calicoq: componentImage('calicoq', releases[0]),
+  },
+};
+
+module.exports = variables;
