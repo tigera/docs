c40f77e507e426dcabf9a414f28dc568a4177215
sujeet-kr <sujeet.kr@hotmail.com>
2025-09-26 10:21:48 -0400

    place emoji outside of link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,7 +105,7 @@ export default async function createAsyncConfig() {
         },
         announcementBar: {
           id: "calico_ebpf",
-          content: 'Master Kubernetes network security with Calico. Join our hybrid event, CalicoCon 2025, on November 10! <a href="https://link.tigera.io/ujbNx">Register now 🚀</a>',
+          content: 'Master Kubernetes network security with Calico. Join our hybrid event, CalicoCon 2025, on November 10! <a href="https://link.tigera.io/ujbNx">Register now </a>🚀',
           backgroundColor: "#FCE181",
           textColor: "#000",
           isCloseable: true
