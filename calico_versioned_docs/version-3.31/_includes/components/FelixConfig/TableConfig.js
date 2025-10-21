f87da03b7f7da3a2f85de8d456a21a4056c49548
Christopher Tauchen <chris.tauchen@tigera.io>
2025-10-21 11:06:47 +0100

    Add FelixConfig component with new params to 3.31 and next

---
+++ calico_versioned_docs/version-3.31/_includes/components/FelixConfig/TableConfig.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import sanitizeHtml from 'sanitize-html';
+import styles from './styles.module.css';
+
+const getSanitizedData = ({ fieldData }) => ({
+  sanitizedDescription: { __html: sanitizeHtml(fieldData.DescriptionHTML) },
+  sanitizedSchema: { __html: sanitizeHtml(fieldData.StringSchemaHTML) },
+});
+
+const TableConfig = ({ fieldData }) => {
+  const { sanitizedDescription, sanitizedSchema } = getSanitizedData({ fieldData });
+
+  return (
+    <table className={styles.felixTable}>
+      <thead>
+        <tr>
+          <th>Attribute</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Key</td>
+          <td>
+            <code>{fieldData.NameConfigFile || 'No Default Value'}</code>
+          </td>
+        </tr>
+        <tr>
+          <td>Description</td>
+          <td dangerouslySetInnerHTML={sanitizedDescription} />
+        </tr>
+        <tr>
+          <td>Schema</td>
+          <td dangerouslySetInnerHTML={sanitizedSchema} />
+        </tr>
+        <tr>
+          <td>Default</td>
+          <td>
+            {fieldData.StringDefault === '' ? (
+              'none'
+            ) : fieldData.GoType === '*v1.Duration' ? (
+              <>
+                <code>{fieldData.StringDefault}</code> ({fieldData.ParsedDefault})
+              </>
+            ) : (
+              <code>{fieldData.StringDefault}</code>
+            )}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default TableConfig;
