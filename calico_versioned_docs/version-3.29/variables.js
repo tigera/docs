4508f1280a9a9408cc46cbea5bc5a017e6761d28
Christopher Tauchen <chris.tauchen@tigera.io>
2025-11-19 13:13:36 +0000

    Publish OSS 3.29.7

--- calico_versioned_docs/version-3.29/variables.js
+++ calico_versioned_docs/version-3.29/variables.js
@@ -1,7 +1,7 @@
 const releases = require('./releases.json');
 
 const variables = {
-  releaseTitle: 'v3.29.6',
+  releaseTitle: 'v3.29.7',
   prodname: 'Calico',
   prodnamedash: 'calico',
   version: 'v3.29',
@@ -17,7 +17,7 @@ const variables = {
   noderunning: 'calico-node',
   rootDirWindows: 'C:\\CalicoWindows',
   ppa_repo_name: 'calico-3.29',
-  manifestsUrl: 'https://raw.githubusercontent.com/projectcalico/calico/v3.29.6',
+  manifestsUrl: 'https://raw.githubusercontent.com/projectcalico/calico/v3.29.7',
   releases,
   registry: '',
   vppbranch: 'v3.29.0',
