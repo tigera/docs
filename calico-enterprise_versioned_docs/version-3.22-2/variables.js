5084414041a5afa8d8930c4fa63d73300177545f
Daniel Fox <dan.fox@tigera.io>
2025-12-15 09:19:03 -0800

    Update references to cnx-node (et all) to remove cnx prefix

--- calico-enterprise_versioned_docs/version-3.22-2/variables.js
+++ calico-enterprise_versioned_docs/version-3.22-2/variables.js
@@ -15,7 +15,7 @@ const variables = {
   windowsScriptsURL: 'https://raw.githubusercontent.com/kubernetes-sigs/sig-windows-tools/master/hostprocess',
   prodnameWindows: 'Calico Enterprise for Windows',
   downloadsurl: 'https://downloads.tigera.io',
-  nodecontainer: 'cnx-node',
+  nodecontainer: 'node',
   noderunning: 'calico-node',
   rootDirWindows: 'C:\\TigeraCalico',
   registry: 'quay.io/',
@@ -25,11 +25,11 @@ const variables = {
   dikastesVersion: releases[0].components.dikastes.version,
   releases,
   imageNames: {
-    node: 'tigera/cnx-node',
+    node: 'tigera/node',
     kubeControllers: 'tigera/kube-controllers',
   },
   componentImage: {
-    cnxNode: componentImage('cnx-node', releases[0]),
+    cnxNode: componentImage('node', releases[0]),
     calicoctl: componentImage('calicoctl', releases[0]),
     calicoq: componentImage('calicoq', releases[0]),
   },
