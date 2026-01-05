548b289865c361f82d42b403cbe39cc414b32b9e
Christopher Tauchen <chris.tauchen@tigera.io>
2026-01-05 09:55:48 +0000

    Add website to skip list

--- __tests__/crawler.test.js
+++ __tests__/crawler.test.js
@@ -196,7 +196,8 @@ test('Crawl the docs and execute tests', async () => {
     'https://docs.openstack.org/kolla-ansible/rocky/user/multi-regions.html',
     'https://archive.istio.io/v1.15/docs/setup/additional-setup/sidecar-injection/#automatic-sidecar-injection',
     'https://istio.io/v1.15/docs/reference/config/security/peer_authentication/',
-    'https://istio.io/v1.15/docs/setup/install/'
+    'https://istio.io/v1.15/docs/setup/install/',
+    'https://sysctl-explorer.net/net/ipv4/fib_multipath_hash_policy/'
   ];
 
   const lc = linkChecker();
