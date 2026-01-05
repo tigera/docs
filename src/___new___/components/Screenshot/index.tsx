90c0023dcbe6ee154b6d6fce02aa832a2033f02a
Christopher Tauchen <chris.tauchen@tigera.io>
2026-01-05 15:32:49 +0000

    Move Whisker screenshot to hero position
    Moving the image helps some viewers understand more quickly what's
    going on with the web console. This commit also modifies the
    Screenshot component to allow us to pass a max width for an image.

--- src/___new___/components/Screenshot/index.tsx
+++ src/___new___/components/Screenshot/index.tsx
@@ -4,13 +4,15 @@ import { Image } from '@chakra-ui/react';
 type ScreenshotProps = {
   src: string;
   alt: string;
+  maxW?: string;
 };
 
-const Screenshot: React.FC<ScreenshotProps> = ({ src, alt }) => (
+const Screenshot: React.FC<ScreenshotProps> = ({ src, alt, maxW }) => (
   <Image
     src={src}
     alt={alt}
     sx={screenshot}
+    maxW={maxW}
   />
 );
 export default Screenshot;
