f88d510f65796bb54336f954fdc4a9e6cd5eab83
Christopher Tauchen <chris.tauchen@tigera.io>
2026-01-26 13:15:53 +0000

    Update variable and releases for CE 3.23 EP1
    Added variables with updated versions. Added placeholder
    values for releases.json.
    
    DOCS-2810

--- calico-enterprise_versioned_docs/version-3.23-1/variables.js
+++ calico-enterprise_versioned_docs/version-3.23-1/variables.js
@@ -1,38 +1,36 @@
 const releases = require('./releases.json');
-const componentImage = require('../src/components/utils/componentImage');
+const componentImage = require('../../src/components/utils/componentImage');
 
 const variables = {
-  releaseTitle: 'master',
+  releaseTitle: 'v3.23.0-1.0',
   prodname: 'Calico Enterprise',
   prodnamedash: 'calico-enterprise',
-  version: 'master',
+  version: 'v3.23',
   openSourceVersion: releases[0].calico.minor_version.slice(1),
-  baseUrl: '/calico-enterprise/next',
-  filesUrl: 'https://downloads.tigera.io/ee/master',
+  baseUrl: '/calico-enterprise/3.23',
+  filesUrl: 'https://downloads.tigera.io/ee/v3.23.0-1.0',
   rpmsUrl: 'https://downloads.tigera.io/ee/rpms/' + releases[0].title.slice(0, 5),
   tutorialFilesURL: 'https://docs.tigera.io/files',
-  tmpScriptsURL: 'https://docs.tigera.io/calico-enterprise/next',
+  tmpScriptsURL: 'https://docs.tigera.io/calico-enterprise/3.23',
   windowsScriptsURL: 'https://raw.githubusercontent.com/kubernetes-sigs/sig-windows-tools/master/hostprocess',
   prodnameWindows: 'Calico Enterprise for Windows',
-  prodnamedashWindows: 'calico-enterprise-for-windows',
   downloadsurl: 'https://downloads.tigera.io',
-  nodecontainer: 'cnx-node',
+  nodecontainer: 'node',
   noderunning: 'calico-node',
   rootDirWindows: 'C:\\TigeraCalico',
-  registry: 'gcr.io/unique-caldron-775/cnx/', // Change to 'quay.io/' for new release
+  registry: 'quay.io/',
   envoyVersion: '1.5.0',
-  chart_version_name: 'master',
+  chart_version_name: 'v3.23.0-1.0-0',
   tigeraOperator: releases[0]['tigera-operator'],
   dikastesVersion: releases[0].components.dikastes.version,
-  manifestsUrl: 'https://docs.tigera.io/master',
   releases,
   imageNames: {
-    node: 'tigera/cnx-node',
+    node: 'tigera/node',
     kubeControllers: 'tigera/kube-controllers',
   },
   componentImage: {
-    cnxNode: componentImage('cnx-node', releases[0]),
-    calicoctl:componentImage('calicoctl', releases[0]),
+    cnxNode: componentImage('node', releases[0]),
+    calicoctl: componentImage('calicoctl', releases[0]),
     calicoq: componentImage('calicoq', releases[0]),
   },
 };
