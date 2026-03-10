a894659e8d7d75ec0b611057cfa859f9519d5226
Alex O'Regan <alex.oregan@tigera.io>
2026-03-10 09:56:34 +0000

    * Documents maglev-conntracks metric for OSS,EE,CE * Documents node maintenance metric for EE,CE * Links maglev metrics from maglev how-to * Documents Node resources new "maintenance" field for EE,CE * Adds how-to for node-maintenance for EE,CE

--- sidebars-calico-enterprise.js
+++ sidebars-calico-enterprise.js
@@ -183,6 +183,7 @@ module.exports = {
             'networking/configuring/node-local-dns-cache',
             'networking/configuring/qos-controls',
             'networking/configuring/add-maglev-load-balancing',
+            'networking/configuring/mark-lb-node-for-maintenance',
           ],
         },
         {
