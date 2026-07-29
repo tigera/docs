0d038430c29bf6ab75560a38a3c8c4ca5a8b0680
Casey Davenport <caseydavenport@users.noreply.github.com>
2026-07-29 07:41:13 -0700

    Fix Enterprise CRD manifest references for native v3 CRD mode (#2882)

--- calico-enterprise/_includes/components/InstallAKS.js
+++ calico-enterprise/_includes/components/InstallAKS.js
@@ -20,7 +20,7 @@ export default function InstallAKS(props) {
       <ol>
         <li>
           <p>Install the Tigera Operator and custom resource definitions.</p>
-          <CodeBlock>kubectl create -f {filesUrl}/manifests/operator-crds.yaml</CodeBlock>
+          <CodeBlock>kubectl create -f {filesUrl}/manifests/v1_crd_projectcalico_org.yaml</CodeBlock>
           <CodeBlock>kubectl create -f {filesUrl}/manifests/tigera-operator.yaml</CodeBlock>
         </li>
         <li>
@@ -131,7 +131,7 @@ spec:
         </li>
         <li>
           <p>Install the Tigera Operator and custom resource definitions.</p>
-          <CodeBlock>kubectl create -f {filesUrl}/manifests/operator-crds.yaml</CodeBlock>
+          <CodeBlock>kubectl create -f {filesUrl}/manifests/v1_crd_projectcalico_org.yaml</CodeBlock>
           <CodeBlock>kubectl create -f {filesUrl}/manifests/tigera-operator.yaml</CodeBlock>
         </li>
         <li>
