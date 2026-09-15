4319ac38cf9dfeeeb6279583110b6485db88bd7e
Chris Tauchen <chris.tauchen@tigera.io>
2026-09-15 16:32:36 +0100

    DOCS-3047: Use the TigeraSecureEnterprise variant where the operator rejects the alias

--- calico-enterprise_versioned_docs/version-3.23-2/_includes/components/PrivateRegistryImagePath.js
+++ calico-enterprise_versioned_docs/version-3.23-2/_includes/components/PrivateRegistryImagePath.js
@@ -138,7 +138,7 @@ kind: Installation
 metadata:
   name: default
 spec:
-  variant: CalicoEnterprise
+  variant: TigeraSecureEnterprise
   imagePullSecrets:
     - name: tigera-pull-secret
   // highlight-next-line
